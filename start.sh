9cfc2758ca4af7ef5db0ea12995cc4034ffbed2e
Johannes Meuer <johannes.meuer@isc-events.com>
2021-07-09 18:44:59 +0200

    set training to radolan

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ####RAW
-python train.py --root /work/bb1152/k204233/climatereconstructionAI/data --batch_size 18 --n_threads 32 --max_iter 500000 --mask_root /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/radclim_missmask.h5 --save_dir ./snapshots/radclim --log_dir ./logs/radclim
+python train.py --root /work/bb1152/k204233/climatereconstructionAI/data --batch_size 18 --n_threads 32 --max_iter 500000 --mask_root /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/radolan_missmask.h5 --save_dir ./snapshots/radolan --log_dir ./logs/radolan
 ####FINE
 #python train.py --root /work/bb1152/k204233/climatereconstructionAI/data --mask_root /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/hadcrut4-missmask.h5 --finetune --resume /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/20cr/ckpt/500000.pth --batch_size 18 --n_threads 4 --max_it$
 ####TEST
