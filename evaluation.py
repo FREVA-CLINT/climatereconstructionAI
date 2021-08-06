6d98080e5c1f70d25d978682cee3d276c7c6431d
Johannes Meuer <johannes.meuer@isc-events.com>
2021-08-06 16:08:02 +0200

    add evaluation2 to train process

--- evaluation.py
+++ evaluation.py
@@ -6,7 +6,7 @@ from util.image import unnormalize
 
 
 def evaluate(model, dataset, device, filename):
-    image, mask, gt = zip(*[dataset[i] for i in range(15)])
+    image, mask, gt = zip(*[dataset[i] for i in range(16)])
     image = torch.stack(image)
     mask = torch.stack(mask)
     gt = torch.stack(gt)
