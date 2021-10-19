8b191be402998f2a3c7af4b407906febc98190e4
Johannes Meuer <johannes.meuer@isc-events.com>
2021-10-19 14:00:09 +0200

    remove image creation for infill

--- evaluation2.py
+++ evaluation2.py
@@ -29,10 +29,10 @@ def evaluate2(model, dataset, device, filename, partitions):
         output_part = output_part.to(torch.device('cpu'))
         output_comp_part = mask_part * image_part + (1 - mask_part) * output_part
 
-        grid = make_grid(
-                torch.cat((unnormalize(image_part), mask_part, unnormalize(output_part),
-                           unnormalize(output_comp_part), unnormalize(gt_part)), dim=0))
-        save_image(grid, filename + str(split) + '.jpg')
+        #grid = make_grid(
+        #        torch.cat((unnormalize(image_part), mask_part, unnormalize(output_part),
+        #                   unnormalize(output_comp_part), unnormalize(gt_part)), dim=0))
+        #save_image(grid, filename + str(split) + '.jpg')
 
         image.append(image_part)
         mask.append(mask_part)
