32ebe70e0f9a5f5d7766f1e9f7b2d6b5eeb18058
Johannes Meuer <johannes.meuer@isc-events.com>
2021-11-03 13:30:58 +0100

    add args to evaluation scripts

--- slurm/start-evaluation-mistral.sh
+++ slurm/start-evaluation-mistral.sh
@@ -12,4 +12,16 @@
 module load cuda/10.0.130
 module load singularity/3.6.1-gcc-9.1.0
 
-singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/evaluate.py
+singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
+ python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/evaluate.py \
+ --device cuda --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-type pr \
+ --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-complete-scaled/ \
+ --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/single_radar_fail.h5 \
+ --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-prev-next/ckpt/200000.pth \
+ --evaluation-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/evaluation/precipitation/radolan-prev-next/ \
+ --prev-next 3 \
+ --partitions 2009 \
+ --infill \
+# --create-images \
+# --create-video \
+# --create-report \
