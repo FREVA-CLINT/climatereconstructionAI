2fd75e8df47d662f7371386b83de0062d675de11
Johannes Meuer <jomeuer@googlemail.com>
2021-11-03 13:30:58 +0100

    add args to evaluation scripts

--- slurm/start-evaluation-levante.sh
+++ slurm/start-evaluation-levante.sh
@@ -11,4 +11,16 @@
 
 module source slurm/setup-modules.txt
 
-singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/evaluate.py
+singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
+ python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/evaluate.py \
+ --device cuda --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-type pr \
+ --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-complete-scaled/ \
+ --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/single_radar_fail.h5 \
+ --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-prev-next/ckpt/200000.pth \
+ --evaluation-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/evaluation/precipitation/radolan-prev-next/ \
+ --prev-next 3 \
+ --partitions 147 \
+ --infill \
+# --create-images \
+# --create-video \
+# --create-report \
