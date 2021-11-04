83e5d2074d37470295207432b2347125b370f4ff
Johannes Meuer <jomeuer@googlemail.com>
2021-11-04 12:50:10 +0100

    fix path

--- start-scripts/start-evaluation-local.sh
+++ start-scripts/start-evaluation-local.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 python /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/evaluate.py \
- --device cpu --image-size 72 --pooling-layers 0 --encoding-layers 3 --data-type tas \
+ --device cpu --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-type pr \
  --data-root-dir ../data/20cr/ \
- --mask-dir masks/hadcrut4-missmask.h5 \
+ --mask-dir masks/single_radar_fail.h5 \
  --snapshot-dir snapshots/temperature/20cr/ckpt/1000.pth \
- --evaluation-dir evaluation/temperature/20cr/ \
- --prev-next 0 \
+ --evaluation-dir evaluation/radolan-prev-next/ \
+ --prev-next 1 \
  --partitions 272 \
- --infill infill \
-# --create-images \
+ --create-images 2017-07-12-14:00,2017-07-12-14:00 \
+# --infill infill \
 # --create-video \
 # --create-report \
