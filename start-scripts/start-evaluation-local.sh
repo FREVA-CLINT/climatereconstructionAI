86acc92187407e5f86996e6acb68caeffafac2cd
Johannes Meuer <johannes.meuer@isc-events.com>
2021-11-06 10:51:05 +0100

    select only mid indexed element for infilling

--- start-scripts/start-evaluation-local.sh
+++ start-scripts/start-evaluation-local.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 python /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/evaluate.py \
- --device cpu --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-type pr \
+ --device cpu --image-size 72 --pooling-layers 0 --encoding-layers 3 --data-type tas \
  --data-root-dir ../data/20cr/ \
- --mask-dir masks/single_radar_fail.h5 \
+ --mask-dir masks/hadcrut4-missmask.h5 \
  --snapshot-dir snapshots/temperature/20cr/ckpt/1000.pth \
- --evaluation-dir evaluation/radolan-prev-next/ \
- --prev-next 1 \
- --partitions 272 \
- --create-images 2017-07-12-14:00,2017-07-12-14:00 \
-# --infill infill \
-# --create-video \
-# --create-report \
+ --evaluation-dir evaluation/temperature/20cr/ \
+ --prev-next 3 \
+ --partitions 1 \
+ --create-images 2000-07-12-14:00,2017-07-12-14:00 \
+ --infill infill \
+ --create-video \
+ --create-report \
