0162b2141db2a9ed9de00635d820b5230e9c3e95
Johannes Meuer <jomeuer@googlemail.com>
2021-11-08 14:34:48 +0100

    fix loss for multi dimensional input

--- opt.py
+++ opt.py
@@ -1,5 +1,7 @@
-MEAN = [0.485, 0.456, 0.406]
-STD = [0.229, 0.224, 0.225]
 
 LAMBDA_DICT = {
-    'valid': 1.0, 'hole': 6.0, 'tv': 0.1, 'prc': 0.05, 'style': 120.0}
+    'valid': 1.0, 'hole': 6.0, 'tv': 0.1, 'prc': 0.05, 'style': 120.0
+}
+#LAMBDA_DICT = {
+#    'ce': 0.1, 'mse': 0.1
+#}
