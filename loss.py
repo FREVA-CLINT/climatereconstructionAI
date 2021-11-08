fde3860f862d49af44105fbd3f89ddd99238013d
Johannes Meuer <johannes.meuer@isc-events.com>
2021-11-08 14:34:48 +0100

    fix loss for multi dimensional input

--- loss.py
+++ loss.py
@@ -22,13 +22,13 @@ class InpaintingLoss(nn.Module):
     def __init__(self, extractor):
         super().__init__()
         self.l1 = nn.L1Loss()
+        self.crossentropy = nn.CrossEntropyLoss()
+        self.mse = nn.MSELoss()
         self.extractor = extractor
 
     def forward(self, input, mask, output, gt, device):
-        loss_dict = {}
-
         # get mid indexed element
-        mid_index = torch.tensor([(output.shape[1] // 2)],dtype=torch.long).to(device)
+        mid_index = torch.tensor([(input.shape[1] // 2)],dtype=torch.long).to(device)
         input = torch.index_select(input, dim=1, index=mid_index)
         gt = torch.index_select(gt, dim=1, index=mid_index)
         mask = torch.index_select(mask, dim=1, index=mid_index)
@@ -36,9 +36,17 @@ class InpaintingLoss(nn.Module):
         # create output_comp
         output_comp = mask * input + (1 - mask) * output
 
+        # define different loss functions from output and output_comp
+        loss_dict = {}
+        #loss_dict['ce'] = 0.0
+        #for i in range(gt.shape[0]):
+        #    print(torch.squeeze(output_comp[i], dim=0).shape)
+        #    loss_dict['ce'] += self.crossentropy(torch.squeeze(output_comp[i], dim=0), torch.squeeze(gt[i], dim=0))
+        #loss_dict['mse'] = self.mse(output_comp, gt)
         loss_dict['hole'] = self.l1((1 - mask) * output, (1 - mask) * gt)
         loss_dict['valid'] = self.l1(mask * output, mask * gt)
 
+        # define different loss function from features from output and output_comp
         feat_output = self.extractor(torch.cat([output] * 3, 1))
         feat_output_comp = self.extractor(torch.cat([output_comp] * 3, 1))
         feat_gt = self.extractor(torch.cat([gt] * 3, 1))
