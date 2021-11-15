a805a83fbfa9915d4ef8d50a46cc5f14a8674c3a
Johannes Meuer <johannes.meuer@isc-events.com>
2021-11-15 18:36:53 +0100

    adjust start script

--- start-scripts/start-training-levante.sh
+++ start-scripts/start-training-levante.sh
@@ -29,7 +29,7 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-lstm/ \
  --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-lstm/ \
  --prev-next 0 \
- --lstm-steps 5 \
+ --lstm-steps 4 \
  --max-iter 200000 \
  --resume /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-lstm/ckpt/100000.pth \
  --finetune
