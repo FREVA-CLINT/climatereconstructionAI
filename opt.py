c9c51a4b713e797c4757937f5bb1e8bf34710a4e
Johannes Meuer <johannes.meuer@isc-events.com>
2021-11-16 14:32:43 +0100

    add SSL loss function

--- opt.py
+++ opt.py
@@ -1,7 +1,7 @@
 
-LAMBDA_DICT = {
-    'valid': 1.0, 'hole': 6.0, 'tv': 0.1, 'prc': 0.05, 'style': 120.0
-}
 #LAMBDA_DICT = {
-#    'pearson': 0.1, 'rmse': 0.1, 's-index': 0.1
+#    'valid': 1.0, 'hole': 6.0, 'tv': 0.1, 'prc': 0.05, 'style': 120.0
 #}
+LAMBDA_DICT = {
+    'SSL': 1.0
+}
