bc9d442df3552ce68f6d95421f4b3f89ca877b4a
Johannes Meuer <jomeuer@googlemail.com>
2021-11-16 14:54:21 +0100

    separate losses for img and pr

--- opt.py
+++ opt.py
@@ -1,7 +1,7 @@
 
-#LAMBDA_DICT = {
-#    'valid': 1.0, 'hole': 6.0, 'tv': 0.1, 'prc': 0.05, 'style': 120.0
-#}
-LAMBDA_DICT = {
+LAMBDA_DICT_IMG_INPAINTING = {
+    'valid': 1.0, 'hole': 6.0, 'tv': 0.1, 'prc': 0.05, 'style': 120.0
+}
+LAMBDA_DICT_PR_INPAINTING = {
     'SSL': 1.0
 }
