0b72668ed1c0f84be6d1512d10a3248cc5c771d8
Johannes Meuer <jomeuer@googlemail.com>
2021-11-16 16:01:39 +0100

    configure lstm steps

--- start-scripts/start-lstm-training-levante.sh
+++ start-scripts/start-lstm-training-levante.sh
@@ -18,17 +18,17 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/single_radar_fail.h5 \
  --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-lstm/ \
  --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-lstm/ \
- --lstm-steps 4 \
+ --lstm-steps 3 \
  --max-iter 100000 \
  --save-model-interval 10000
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
- python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train.py \
+ python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/trainLSTM.py \
  --device cuda --batch-size 4 --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-type pr \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-complete-scaled/ \
  --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/single_radar_fail.h5 \
  --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-lstm/ \
  --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-lstm/ \
- --lstm-steps 4 \
+ --lstm-steps 3 \
  --max-iter 200000 \
  --resume /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-lstm/ckpt/100000.pth \
  --finetune \
