4d015abc407a0546bc7296f14e94390ae7a9625c
Johannes Meuer <jomeuer@googlemail.com>
2021-11-18 18:37:26 +0100

    change loss to pr

--- trainMultiCUNet.py
+++ trainMultiCUNet.py
@@ -94,7 +94,7 @@ else:
 
 # define optimizer and loss functions
 optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=lr)
-criterion = PrevNextInpaintingLoss(VGG16FeatureExtractor()).to(device)
+criterion = PrecipitationInpaintingLoss().to(device)
 
 # define start point
 start_iter = 0
@@ -114,7 +114,7 @@ for i in tqdm(range(start_iter, args.max_iter)):
 
     # calculate loss function and apply backpropagation
     loss = 0.0
-    for key, coef in opt.LAMBDA_DICT_IMG_INPAINTING.items():
+    for key, coef in opt.LAMBDA_DICT_PR_INPAINTING.items():
         value = coef * loss_dict[key]
         loss += value
         if (i + 1) % args.log_interval == 0:
