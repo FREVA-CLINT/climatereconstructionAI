8a7cbfe4df7e0edd4ad18450e726f574d09a9f54
Johannes Meuer <johannes.meuer@isc-events.com>
2021-11-18 13:09:42 +0100

    FIX

--- start-scripts/start-lstm-evaluation-mistral.sh
+++ start-scripts/start-lstm-evaluation-mistral.sh
@@ -21,7 +21,7 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-lstm/ckpt/100000.pth \
  --evaluation-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/evaluation/precipitation/radolan-lstm/ \
  --lstm-steps 3 \
- --partitions 6027 \
+ --partitions 2009 \
  --infill test \
 # --create-images 2017-07-12-14:00,2017-07-12-14:00 \
 # --create-video \
