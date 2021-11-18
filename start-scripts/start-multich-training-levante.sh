21253a6365f6e5039d958eb72dd3e5d04298f342
Johannes Meuer <johannes.meuer@isc-events.com>
2021-11-18 10:11:41 +0100

    change start script

--- start-scripts/start-multich-training-levante.sh
+++ start-scripts/start-multich-training-levante.sh
@@ -11,15 +11,15 @@
 
 module source start-scripts/setup-modules.txt
 
-singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
- python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/trainMultiCUNet.py \
- --device cuda --batch-size 4 --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-type pr \
- --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-complete-scaled/ \
- --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/single_radar_fail.h5 \
- --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-ssl/ \
- --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-ssl/ \
- --prev-next 1 \
- --max-iter 100000
+#singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
+# python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/trainMultiCUNet.py \
+# --device cuda --batch-size 4 --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-type pr \
+# --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-complete-scaled/ \
+# --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/single_radar_fail.h5 \
+# --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-ssl/ \
+# --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-ssl/ \
+# --prev-next 1 \
+# --max-iter 100000
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/trainMultiCUNet.py \
  --device cuda --batch-size 4 --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-type pr \
