9a7dc123486bda2a519ef7ef63f0bdcac4b1cc4c
Johannes Meuer <jomeuer@googlemail.com>
2021-11-18 18:28:05 +0100

    fix

--- trainMultiCUNet.py
+++ trainMultiCUNet.py
@@ -43,9 +43,9 @@ device = torch.device(args.device)
 
 def evaluate(model, dataset, device, filename):
     image, mask, gt = zip(*[dataset[i] for i in range(8)])
-    image = torch.stack(image)
-    mask = torch.stack(mask)
-    gt = torch.stack(gt)
+    image = torch.stack(image).to(device)
+    mask = torch.stack(mask).to(device)
+    gt = torch.stack(gt).to(device)
 
     with torch.no_grad():
         output, _ = model(image.to(device), mask.to(device))
