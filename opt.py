d3bea48f00d5190002c62f7a129d26e555470343
Johannes Meuer <jomeuer@googlemail.com>
2021-11-19 10:24:51 +0100

    change loss to img

--- opt.py
+++ opt.py
@@ -3,5 +3,5 @@ LAMBDA_DICT_IMG_INPAINTING = {
     'valid': 1.0, 'hole': 6.0, 'tv': 0.1, 'prc': 0.05, 'style': 120.0
 }
 LAMBDA_DICT_PR_INPAINTING = {
-    'L1-OUT': 0.05, 'L1-OUT-COMP': 0.05, 'HOLE': 6.0, 'STYLE-OUT': 50, 'STYLE-OUT-COMP': 50
+    'valid': 1.0, 'hole': 6.0, 'tv': 0.1, 'prc': 0.05, 'style': 120.0
 }
