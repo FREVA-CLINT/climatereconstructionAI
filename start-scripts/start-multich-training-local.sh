39465fdf3767cb00aae3eb7663aab7cdde99260f
Johannes Meuer <jomeuer@googlemail.com>
2021-11-24 15:17:00 +0100

    update loss function

--- start-scripts/start-multich-training-local.sh
+++ start-scripts/start-multich-training-local.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/trainMultiCUNet.py \
- --device cpu --batch-size 4 --image-size 72 --pooling-layers 0 --encoding-layers 3 --data-type tas \
+ --device cpu --batch-size 18 --image-size 72 --pooling-layers 0 --encoding-layers 3 --data-type tas \
  --data-root-dir ../data/20cr/ \
  --mask-dir masks/hadcrut4-missmask.h5 \
  --snapshot-dir snapshots/temperature/20cr/ \
