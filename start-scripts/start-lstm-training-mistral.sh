b6aee48530963e8a85cdfdf29d0c309a9deb39d7
Johannes Meuer <johannes.meuer@isc-events.com>
2021-11-24 10:46:42 +0100

    update start scripts

--- start-scripts/start-lstm-training-mistral.sh
+++ start-scripts/start-lstm-training-mistral.sh
@@ -14,11 +14,11 @@ module load singularity/3.6.1-gcc-9.1.0
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/trainLSTM.py \
- --device cuda --batch-size 2 --image-size 72 --pooling-layers 0 --encoding-layers 3 --data-type tas \
+ --device cuda --batch-size 4 --image-size 72 --pooling-layers 0 --encoding-layers 3 --data-type tas \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/20cr/ \
  --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/hadcrut4-missmask.h5 \
  --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/temperature/20cr-lstm/ \
- --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/temperature/20cr-lstm// \
+ --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/temperature/20cr-lstm/ \
  --lstm-steps 0 \
  --max-iter 1000000 \
  --save-model-interval 10000
