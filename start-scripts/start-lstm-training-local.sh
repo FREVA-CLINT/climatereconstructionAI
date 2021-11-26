9a98f08eea95d842c438d71fd8883d25a8fd015f
Johannes Meuer <jomeuer@googlemail.com>
2021-11-26 18:15:41 +0100

    resturcutre lstm network

--- start-scripts/start-lstm-training-local.sh
+++ start-scripts/start-lstm-training-local.sh
@@ -3,8 +3,8 @@
 python /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/trainLSTM.py \
  --device cpu --batch-size 2 --image-size 72 --pooling-layers 0 --encoding-layers 3 --data-type tas \
  --data-root-dir /home/joe/PycharmProjects/climatereconstructionAI/data/20cr/ \
- --mask-dir /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/masks/hadcrut4-missmask.h5 \
- --snapshot-dir /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/snapshots/temperature/20cr-lstm/ \
+ --mask-dir /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/masks/single_temp_mask.h5 \
+ --snapshot-dir /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/snapshots/temperature/20cr-lstm-test1/ \
  --log-dir /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/logs/temperature/20cr-lstm// \
  --lstm-steps 3 \
  --max-iter 1000000 \
