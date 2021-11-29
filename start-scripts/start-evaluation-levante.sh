289896d9601affdbf6cc7689c8e47f4778565629
Johannes Meuer <johannes.meuer@isc-events.com>
2021-11-29 14:23:53 +0100

    change eval script

--- start-scripts/start-evaluation-levante.sh
+++ start-scripts/start-evaluation-levante.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=128
 #SBATCH --time=12:00:00
 #SBATCH --mem=256G
-#SBATCH --nodelist=vader1
+#SBATCH --nodelist=vader3
 
 module source start-scripts/setup-modules.txt
 
@@ -16,11 +16,11 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --device cuda --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-type pr \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-complete-scaled/ \
  --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/single_radar_fail.h5 \
- --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-prev-next/ckpt/200000.pth \
- --evaluation-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/evaluation/precipitation/radolan-prev-next/ \
- --lstm-steps 1 \
- --partitions 147 \
+ --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-lstm/ckpt/200000.pth \
+ --evaluation-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/evaluation/precipitation/radolan-lstm/ \
+ --lstm-steps 3 \
+ --partitions 2009 \
  --infill test \
-# --create-images 2017-07-12-14:00,2017-07-12-14:00 \
-# --create-video \
-# --create-report \
+ --create-images 2017-07-12-14:00,2017-07-12-14:00 \
+ --create-video \
+ --create-report \
