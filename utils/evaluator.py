aba0cf6160856fc9460263eb38ba1a41a6ed0ae0
Johannes Meuer <jomeuer@googlemail.com>
2021-11-30 17:29:45 +0100

    add fldcor to evaluator

--- utils/evaluator.py
+++ utils/evaluator.py
@@ -163,7 +163,7 @@ class PConvLSTMEvaluator:
                     image = imageio.imread(self.eval_save_dir + 'images/' + file + '_' + str(i) + '.jpg')
                     writer.append_data(image)
 
-    def create_evaluation_report(self, save_dir='evaluations/', create_evalutation_files=False, clean_data=True,
+    def create_evaluation_report(self, save_dir='evaluations/', create_evalutation_files=True, clean_data=True,
                                  infilled=True):
         if not os.path.exists(self.eval_save_dir + save_dir):
             os.makedirs('{:s}'.format(self.eval_save_dir + save_dir))
