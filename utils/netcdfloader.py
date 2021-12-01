6cb614f02e765348be4f51f6d54e7fb5b7d68ade
Johannes Meuer <jomeuer@googlemail.com>
2021-12-01 13:51:03 +0100

    add multi channel input support

--- utils/netcdfloader.py
+++ utils/netcdfloader.py
@@ -30,168 +30,91 @@ class InfiniteSampler(data.sampler.Sampler):
 
 
 class NetCDFLoader(torch.utils.data.Dataset):
-    def __init__(self, img_root, mask_root, split, data_type):
+    def __init__(self, data_root, img_names, mask_root, mask_names, split, data_types, lstm_steps):
         super(NetCDFLoader, self).__init__()
+        assert len(img_names) == len(mask_names) == len(data_types)
 
         self.split = split
-        self.data_type = data_type
+        self.data_types = data_types
+        self.img_names = img_names
+        self.mask_names = mask_names
+        self.lstm_steps = lstm_steps
 
         if split == 'train':
-            self.img_path = glob('{:s}/data_large/*.h5'.format(img_root),
-                              recursive=True)[0]
+            self.data_path = '{:s}/data_large/'.format(data_root)
         elif split == 'test' or split == 'infill':
-            self.img_path = glob('{:s}/test_large/*.h5'.format(img_root))[0]
+            self.data_path = '{:s}/test_large/'.format(data_root)
         elif split == 'val':
-            self.img_path = glob('{:s}/val_large/*.h5'.format(img_root))[0]
+            self.data_path = '{:s}/val_large/'.format(data_root)
         self.mask_path = mask_root
 
-        # define length of image and mask
-        img_file = h5py.File('{:s}'.format(self.img_path), 'r')
-        img_data = img_file.get(self.data_type)
-        self.img_length = len(img_data[:, 1, 1])
-        mask_file = h5py.File('{:s}'.format(self.mask_path), 'r')
-        mask_data = mask_file.get(self.data_type)
-        self.mask_length = len((mask_data[:, 1, 1]))
+        img_lengths = []
+        self.mask_lengths = []
+        # define lengths of image and mask
+        for i in range(len(self.data_types)):
+            img_file = h5py.File('{}{}'.format(self.data_path, self.img_names[i]), 'r')
+            img_data = img_file.get(self.data_types[i])
+            img_lengths.append(len(img_data[:, 1, 1]))
+            mask_file = h5py.File('{}{}'.format(self.mask_path, self.mask_names[i]), 'r')
+            mask_data = mask_file.get(self.data_types[i])
+            self.mask_lengths.append(len((mask_data[:, 1, 1])))
+
+        # check if images all have same length
+        assert img_lengths[:-1] == img_lengths[1:]
+        self.img_length = img_lengths[0]
 
     def load_data(self):
         # open netcdf file for img and mask
-        img_file = h5py.File('{:s}'.format(self.img_path), 'r')
-        img_data = img_file.get(self.data_type)
-        mask_file = h5py.File(self.mask_path)
-        mask_data = mask_file.get(self.data_type)
-
+        img_data = []
+        mask_data = []
+        for i in range(len(self.data_types)):
+            img_file = h5py.File('{}{}'.format(self.data_path, self.img_names[i]), 'r')
+            img_data.append(img_file.get(self.data_types[i]))
+            mask_file = h5py.File('{}{}'.format(self.mask_path, self.mask_names[i]))
+            mask_data.append(mask_file.get(self.data_types[i]))
         return img_data, mask_data
+
+    def __getitem__(self, index):
+        img_data, mask_data = self.load_data()
+
+        img_total = []
+        mask_total = []
+
+        for i in range(len(img_data)):
+            img_inner = []
+            mask_inner = []
+            for j in range(self.lstm_steps + 1):
+                # img
+                new_index = index - j
+                if new_index < 0:
+                    new_index = 0
+                # get next img
+                img_prev = img_data[i][new_index, :, :]
+                img_prev = torch.from_numpy(img_prev[:, :])
+                img_prev = img_prev.unsqueeze(0)
+                img_inner.insert(0, img_prev)
+
+                # mask
+                new_index = index - j
+                if new_index < 0:
+                    new_index = 0
+                # get next mask
+                if self.split == 'infill':
+                    mask_prev = mask_data[i][new_index, :, :]
+                else:
+                    mask_prev = mask_data[i][random.randint(0, self.mask_lengths[i] - 1), :, :]
+                mask_prev = torch.from_numpy(mask_prev[:, :])
+                mask_prev = mask_prev.unsqueeze(0)
+                mask_inner.insert(0, mask_prev)
+            img_inner = torch.cat(img_inner)
+            mask_inner = torch.cat(mask_inner)
+            img_total.append(img_inner)
+            mask_total.append(mask_inner)
+
+        img_total = torch.stack(img_total, dim=1)
+        mask_total = torch.stack(mask_total, dim=1)
+
+        return img_total * mask_total, mask_total, img_total
 
     def __len__(self):
         return self.img_length
-
-
-class SimpleNetCDFDataLoader(NetCDFLoader):
-    def __getitem__(self, index):
-        img_data, mask_data = self.load_data()
-
-        # get img and mask from index
-        img_current = img_data[index, :, :]
-        img_current = torch.from_numpy(img_current[:, :])
-        img_current = img_current.unsqueeze(0)
-        if self.split == 'infill':
-            mask_current = mask_data[index, :, :]
-        else:
-            mask_current = mask_data[random.randint(0, self.mask_length - 1), :, :]
-        mask_current = torch.from_numpy(mask_current[:, :])
-        mask_current = mask_current.unsqueeze(0)
-
-        img_total = torch.cat([img_current])
-        mask_total = torch.cat([mask_current])
-
-        return img_total * mask_total, mask_total, img_total
-
-
-class PrevNextNetCDFDataLoader(NetCDFLoader):
-    def __init__(self, img_root, mask_root, split, data_type, prev_next):
-        super(PrevNextNetCDFDataLoader, self).__init__(img_root, mask_root, split, data_type)
-        self.prev_next = prev_next
-
-    def __getitem__(self, index):
-        img_data, mask_data = self.load_data()
-
-        # get img and mask from index
-        img_current = img_data[index, :, :]
-        img_current = torch.from_numpy(img_current[:, :])
-        img_current = img_current.unsqueeze(0)
-        if self.split == 'infill':
-            mask_current = mask_data[index, :, :]
-        else:
-            mask_current = mask_data[random.randint(0, self.mask_length - 1), :, :]
-        mask_current = torch.from_numpy(mask_current[:, :])
-        mask_current = mask_current.unsqueeze(0)
-        # get images previous and next to image
-        img_total = []
-        img_total.append(img_current)
-        mask_total = []
-        mask_total.append(mask_current)
-        for i in range(1, self.prev_next+1):
-            # img
-            prev_index = index-i
-            if prev_index < 0:
-                prev_index = 0
-            next_index = index+i
-            if next_index > self.img_length-1:
-                next_index = self.img_length-1
-            # get prev img
-            img_prev = img_data[prev_index, :, :]
-            img_prev = torch.from_numpy(img_prev[:, :])
-            img_prev = img_prev.unsqueeze(0)
-            img_total.insert(0, img_prev)
-            # get next img
-            img_next = img_data[next_index, :, :]
-            img_next = torch.from_numpy(img_next[:, :])
-            img_next = img_next.unsqueeze(0)
-            img_total.append(img_next)
-
-            # mask
-            next_index = index+i
-            if next_index > self.mask_length-1:
-                next_index = self.mask_length-1
-            # get prev mask
-            if self.split == 'infill':
-                mask_prev = mask_data[prev_index, :, :]
-            else:
-                mask_prev = mask_data[random.randint(0, self.mask_length - 1), :, :]
-            mask_prev = torch.from_numpy(mask_prev[:, :])
-            mask_prev = mask_prev.unsqueeze(0)
-            # get next mask
-            if self.split == 'infill':
-                mask_next = mask_data[next_index, :, :]
-            else:
-                mask_next = mask_data[random.randint(0, self.mask_length - 1), :, :]
-            mask_next = torch.from_numpy(mask_next[:, :])
-            mask_next = mask_next.unsqueeze(0)
-            mask_total.insert(0, mask_prev)
-            mask_total.append(mask_next)
-        img_total = torch.cat(img_total)
-        mask_total = torch.cat(mask_total)
-
-        return img_total * mask_total, mask_total, img_total
-
-
-class LSTMNetCDFDataLoader(NetCDFLoader):
-    def __init__(self, img_root, mask_root, split, data_type, lstm_steps):
-        super(LSTMNetCDFDataLoader, self).__init__(img_root, mask_root, split, data_type)
-        self.lstm_steps = lstm_steps
-
-    def __getitem__(self, index):
-        img_data, mask_data = self.load_data()
-
-        img_total = []
-        mask_total = []
-        for i in range(self.lstm_steps + 1):
-            # img
-            new_index = index - i
-            if new_index < 0:
-                new_index = 0
-            # get next img
-            img_prev = img_data[new_index, :, :]
-            img_prev = torch.from_numpy(img_prev[:, :])
-            img_prev = img_prev.unsqueeze(0)
-            img_total.insert(0, img_prev)
-
-            # mask
-            new_index = index - i
-            if new_index < 0:
-                new_index = 0
-            # get next mask
-            if self.split == 'infill':
-                mask_prev = mask_data[new_index, :, :]
-            else:
-                mask_prev = mask_data[random.randint(0, self.mask_length - 1), :, :]
-            mask_prev = torch.from_numpy(mask_prev[:, :])
-            mask_prev = mask_prev.unsqueeze(0)
-            mask_total.insert(0, mask_prev)
-        img_total = torch.cat(img_total)
-        mask_total = torch.cat(mask_total)
-
-        img_total = torch.unsqueeze(img_total, 1)
-        mask_total = torch.unsqueeze(mask_total, 1)
-
-        return img_total * mask_total, mask_total, img_total
