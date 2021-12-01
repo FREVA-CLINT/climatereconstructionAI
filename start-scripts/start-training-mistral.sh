745d5f3dc4b0745a9eba215a907d5f94dbb65d40
Johannes Meuer <jomeuer@googlemail.com>
2021-12-01 16:25:44 +0100

    add multi-channel support in readme

--- start-scripts/start-training-mistral.sh
+++ start-scripts/start-training-mistral.sh
@@ -14,25 +14,25 @@ module load singularity/3.6.1-gcc-9.1.0
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
- --device cuda --batch-size 4 --image-size 72 --pooling-layers 0 --encoding-layers 3 --data-type tas \
- --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/20cr/ \
- --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/single_temp_mask.h5 \
- --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/temperature/20cr-lstm-Pconv/ \
- --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/temperature/20cr-lstm/ \
- --lstm-steps 3 \
- --max-iter 1000000 \
- --save-model-interval 10000 \
- --log-interval 1000
+ --device cuda --batch-size 4 --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr,tas \
+ --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-complete-scaled/ \
+ --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
+ --img-names radolan.h5,rea2-tas.h5 --mask-names single_radar_fail.h5,masks_ones_tas.h5 \
+ --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-rea2-tas/ \
+ --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-rea2-tas/ \
+ --lstm-steps 0 \
+ --max-iter 100000 \
+ --save-model-interval 10000
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
- --device cuda --batch-size 4 --image-size 72 --pooling-layers 0 --encoding-layers 3 --data-type tas \
- --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/20cr/ \
- --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/single_temp_mask.h5 \
- --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/temperature/20cr-lstm-Pconv/ \
- --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/temperature/20cr-lstm/ \
- --resume /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/temperature/20cr-lstm/100000.pth \
+ --device cuda --batch-size 4 --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr,tas \
+ --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-complete-scaled/ \
+ --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
+ --img-names radolan.h5,rea2-tas.h5 --mask-names single_radar_fail.h5,masks_ones_tas.h5 \
+ --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-rea2-tas/ \
+ --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-rea2-tas/ \
+ --lstm-steps 0 \
+ --max-iter 200000 \
+ --resume /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-rea2-tas/ckpt/200000.pth \
  --finetune \
- --lstm-steps 3 \
- --max-iter 2000000 \
- --save-model-interval 10000 \
- --log-interval 1000
+ --save-model-interval 10000
