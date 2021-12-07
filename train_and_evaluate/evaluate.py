e5a930218c69415855803da2a04a8990a6b032d3
Johannes Meuer <johannes.meuer@isc-events.com>
2021-12-07 15:30:23 +0100

    convert evaluation from cdo to numpy

--- train_and_evaluate/evaluate.py
+++ train_and_evaluate/evaluate.py
@@ -3,17 +3,18 @@ import sys
 sys.path.append('./')
 
 from model.PConvLSTM import PConvLSTM
-from utils.evaluator import PConvLSTMEvaluator
+from utils.evaluation import *
 from utils.netcdfloader import NetCDFLoader
 from utils.io import load_ckpt
 import config as cfg
 
 cfg.set_evaluation_args()
-
-evaluator = PConvLSTMEvaluator(cfg.evaluation_dir, cfg.mask_dir+cfg.mask_names[0], cfg.data_root_dir + 'test_large/', cfg.data_types[0])
+gt = None
+outputs = None
 
 if cfg.infill:
-    dataset_val = NetCDFLoader(cfg.data_root_dir, cfg.img_names, cfg.mask_dir, cfg.mask_names, cfg.infill, cfg.data_types, cfg.lstm_steps)
+    dataset_val = NetCDFLoader(cfg.data_root_dir, cfg.img_names, cfg.mask_dir, cfg.mask_names, cfg.infill,
+                               cfg.data_types, cfg.lstm_steps)
     lstm = True
     if cfg.lstm_steps == 0:
         lstm = False
@@ -27,7 +28,7 @@ if cfg.infill:
 
     model.eval()
 
-    evaluator.infill(model, dataset_val, cfg.partitions)
+    gt, output = infill(model, dataset_val, cfg.partitions)
 
 if cfg.create_images:
     start_date = cfg.create_images.split(',')[0]
@@ -35,10 +36,22 @@ if cfg.create_images:
     create_video = False
     if cfg.create_video:
         create_video = True
-    evaluator.create_evaluation_images('image.nc', create_video, start_date, end_date)
-    evaluator.create_evaluation_images('gt.nc', create_video, start_date, end_date)
-    evaluator.create_evaluation_images('output.nc', create_video, start_date, end_date)
-    evaluator.create_evaluation_images('output_comp.nc', create_video, start_date, end_date)
+    create_evaluation_images('image.nc', create_video, start_date, end_date)
+    create_evaluation_images('gt.nc', create_video, start_date, end_date)
+    create_evaluation_images('output.nc', create_video, start_date, end_date)
+    create_evaluation_images('output_comp.nc', create_video, start_date, end_date)
 
 if cfg.create_report:
-    evaluator.create_evaluation_report()
+    if gt is None or outputs is None:
+        gt = h5py.File('{}{}'.format(cfg.evaluation_dirs[0], 'gt'), 'r').get(cfg.data_types[0])[:, :, :]
+        mask = h5py.File('{}{}'.format(cfg.evaluation_dirs[0], 'mask'), 'r').get(cfg.data_types[0])[:, :, :]
+        if cfg.mask_zero:
+            mask[gt < cfg.mask_zero] = 1
+        gt = ma.masked_array(gt, mask)[:, :, :]
+        outputs = {}
+        for i in range(len(cfg.evaluation_dirs)):
+            output = h5py.File('{}{}'.format(cfg.evaluation_dirs[i], 'output'), 'r').get(cfg.data_types[0])[:, :, :]
+            output = ma.masked_array(output, mask)[:, :, :]
+            output[output < 0.0] = 0.0
+            outputs[cfg.eval_names[i]] = output
+    create_evaluation_report(gt, outputs)
