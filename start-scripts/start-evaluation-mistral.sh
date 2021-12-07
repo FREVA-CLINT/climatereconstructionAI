cd17cceb54d0a1bd82ea941980e59c7617266e59
Johannes Meuer <johannes.meuer@isc-events.com>
2021-12-07 18:10:07 +0100

    add old evaluation system

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -14,7 +14,7 @@ module load singularity/3.6.1-gcc-9.1.0
 module load cdo
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
- python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate2.py \
+ python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate.py \
  --device cuda --image-size 72 --pooling-layers 0 --encoding-layers 3 --data-types pr \
  --img-names tas_20cr_RECONSTRUCT_187001-200512.h5 --mask-names single_radar_fail.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-complete-scaled/ \
