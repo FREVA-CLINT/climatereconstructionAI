b2d072e6eb51b53cf68424c3770936b1bed02235
Johannes Meuer <jomeuer@googlemail.com>
2021-12-07 19:32:44 +0100

    add range to eval

--- train_and_evaluate/evaluate.py
+++ train_and_evaluate/evaluate.py
@@ -32,23 +32,20 @@ if cfg.infill:
     outputs = {cfg.eval_names[0]: output}
 
 if cfg.create_report:
+    r = (119,130)
     if gt is None or outputs is None:
-        gt = h5py.File('{}{}'.format(cfg.evaluation_dirs[0], 'gt'), 'r').get(cfg.data_types[0])[:, :, :]
-        mask = h5py.File('{}{}'.format(cfg.evaluation_dirs[0], 'mask'), 'r').get(cfg.data_types[0])[:, :, :]
+        gt = h5py.File('{}{}'.format(cfg.evaluation_dirs[0], 'gt'), 'r').get(cfg.data_types[0])[r[0]:r[1], :, :]
+        mask = h5py.File('{}{}'.format(cfg.evaluation_dirs[0], 'mask'), 'r').get(cfg.data_types[0])[r[0]:r[1], :, :]
         if gt.ndim == 4:
             gt = gt[:, 0, :, :]
         if mask.ndim == 4:
             mask = mask[:, 0, :, :]
         if cfg.mask_zero:
             mask[gt < cfg.mask_zero] = 1
-        mask = np.delete(mask, 120, 0)
-        gt = np.delete(gt, 120, 0)
-
         gt = ma.masked_array(gt, mask)[:, :, :]
         outputs = {}
         for i in range(len(cfg.evaluation_dirs)):
-            output = h5py.File('{}{}'.format(cfg.evaluation_dirs[i], 'output'), 'r').get(cfg.data_types[0])[:, :, :]
-            output = np.delete(output, 120, 0)
+            output = h5py.File('{}{}'.format(cfg.evaluation_dirs[i], 'output'), 'r').get(cfg.data_types[0])[r[0]:r[1], :, :]
             output = ma.masked_array(output, mask)[:, :, :]
             output[output < 0.0] = 0.0
             if output.ndim == 4:
