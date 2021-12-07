08e86851404b6ab4089d59ba89494137dd69ff33
Johannes Meuer <jomeuer@googlemail.com>
2021-12-07 18:25:17 +0100

    add range to eval

--- train_and_evaluate/evaluate.py
+++ train_and_evaluate/evaluate.py
@@ -32,7 +32,7 @@ if cfg.infill:
     outputs = {cfg.eval_names[0]: output}
 
 if cfg.create_report:
-    range = (0,100)
+    r = (0,100)
     if gt is None or outputs is None:
         gt = h5py.File('{}{}'.format(cfg.evaluation_dirs[0], 'gt'), 'r').get(cfg.data_types[0])[:, :, :]
         mask = h5py.File('{}{}'.format(cfg.evaluation_dirs[0], 'mask'), 'r').get(cfg.data_types[0])[:, :, :]
@@ -42,11 +42,11 @@ if cfg.create_report:
             mask = mask[:, 0, :, :]
         if cfg.mask_zero:
             mask[gt < cfg.mask_zero] = 1
-        gt = ma.masked_array(gt, mask)[range[0]:range[1], :, :]
+        gt = ma.masked_array(gt, mask)[r[0]:r[1], :, :]
         outputs = {}
         for i in range(len(cfg.evaluation_dirs)):
             output = h5py.File('{}{}'.format(cfg.evaluation_dirs[i], 'output'), 'r').get(cfg.data_types[0])[:, :, :]
-            output = ma.masked_array(output, mask)[range[0]:range[1], :, :]
+            output = ma.masked_array(output, mask)[r[0]:r[1], :, :]
             output[output < 0.0] = 0.0
             if output.ndim == 4:
                 output = output[:, 0, :, :]
