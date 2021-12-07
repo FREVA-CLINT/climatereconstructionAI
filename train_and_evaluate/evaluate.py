07142e4c7cb21116221b54747b3a77df72f870ac
Johannes Meuer <jomeuer@googlemail.com>
2021-12-07 19:29:09 +0100

    add range to eval

--- train_and_evaluate/evaluate.py
+++ train_and_evaluate/evaluate.py
@@ -41,16 +41,18 @@ if cfg.create_report:
             mask = mask[:, 0, :, :]
         if cfg.mask_zero:
             mask[gt < cfg.mask_zero] = 1
+        mask = np.delete(mask, 120, 0)
+
         gt = ma.masked_array(gt, mask)[:, :, :]
         gt = np.delete(gt, 120, 0)
         outputs = {}
         for i in range(len(cfg.evaluation_dirs)):
             output = h5py.File('{}{}'.format(cfg.evaluation_dirs[i], 'output'), 'r').get(cfg.data_types[0])[:, :, :]
+            output = np.delete(output, 120, 0)
             output = ma.masked_array(output, mask)[:, :, :]
             output[output < 0.0] = 0.0
             if output.ndim == 4:
                 output = output[:, 0, :, :]
-            output = np.delete(output, 120, 0)
             outputs[cfg.eval_names[i]] = output
     create_evaluation_report(gt, outputs)
 
