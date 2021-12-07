9a9782acec60c038ded279b3194c67b770be1375
Johannes Meuer <johannes.meuer@isc-events.com>
2021-12-07 16:13:52 +0100

    change start script

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -15,16 +15,17 @@ module load cdo
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate.py \
- --device cuda --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr,tas \
- --img-names radolan.h5,rea2-tas.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
+ --device cuda --image-size 72 --pooling-layers 0 --encoding-layers 3 --data-types tas \
+ --img-names tas_20cr_RECONSTRUCT_187001-200512.h5 --mask-names hadcrut_missmask_187001-200512.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
- --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-rea2-tas/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-single-radar-fail/,evaluation/precipitation/radolan-prev-next/,evaluation/precipitation/radolan-lstm/ \
+ --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/temperature/cmip/1000000.pth \
+ --evaluation-dirs evaluation/temperature/cmip/ \
  --lstm-steps 0 \
  --partitions 1177 \
  --create-report \
  --eval-names Simple,Prev-Next,LSTM \
+ --infill infill \
+ --create-report \
 # --create-images 2017-07-12-14:00,2017-07-12-14:00 \
 # --create-video \
-# --create-report \
