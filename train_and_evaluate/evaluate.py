1af3cf6c1fa09302f3bbca2c9d4daf45e684456d
Johannes Meuer <johannes.meuer@isc-events.com>
2021-12-07 15:48:16 +0100

    fix dimension issue

--- train_and_evaluate/evaluate.py
+++ train_and_evaluate/evaluate.py
@@ -48,12 +48,12 @@ if cfg.create_report:
         if cfg.mask_zero:
             mask[gt < cfg.mask_zero] = 1
         gt = ma.masked_array(gt, mask)[:, :, :]
-        print(gt.shape)
         outputs = {}
         for i in range(len(cfg.evaluation_dirs)):
             output = h5py.File('{}{}'.format(cfg.evaluation_dirs[i], 'output'), 'r').get(cfg.data_types[0])[:, :, :]
             output = ma.masked_array(output, mask)[:, :, :]
             output[output < 0.0] = 0.0
+            if output.ndim == 4:
+                output = output[:, 0, :, :]
             outputs[cfg.eval_names[i]] = output
-            print(output.shape)
     create_evaluation_report(gt, outputs)
