4532a9c8e249ee25c8d49c9d27c18c51958c8f0c
Johannes Meuer <johannes.meuer@isc-events.com>
2021-12-08 19:05:15 +0100

    fix

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -228,7 +228,7 @@ def create_evaluation_report(gt, outputs):
     for output_name,output in outputs.items():
         labels.append(output_name)
         data.append(np.sum(output, axis=(1, 2)))
-    labels.append(['GT'])
+    labels.append('GT')
     data.append(np.sum(gt, axis=(1, 2)))
     plt.hist(data, bins=cfg.PDF_BINS, label=labels, edgecolor='black')
 
