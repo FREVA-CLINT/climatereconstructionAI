5147ca0a5c25f7f23bea7f907adf3a35b538c9b9
Johannes Meuer <jomeuer@googlemail.com>
2021-12-09 09:09:09 +0100

    change start script

--- start-scripts/start-evaluation-levante.sh
+++ start-scripts/start-evaluation-levante.sh
@@ -13,16 +13,16 @@ module source start-scripts/setup-modules.txt
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate.py \
- --device cuda --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr,tas \
- --img-names radolan.h5,rea2-tas.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
+ --device cuda --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
+ --img-names radolan.h5 --mask-names single_radar_fail.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
- --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
- --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-rea2-tas/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-single-radar-fail/,evaluation/precipitation/radolan-prev-next/,evaluation/precipitation/radolan-lstm/ \
+ --mask-dir masks/ \
+ --snapshot-dir snapshots/precipitation/radolan-simple-2007-2013/ckpt/200000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-simple-2007-2013/ \
  --lstm-steps 0 \
  --partitions 1177 \
+ --eval-names Simple \
+ --infill test \
  --create-report \
- --eval-names Simple,Prev-Next,LSTM \
-# --infill test \
 # --create-images 2017-07-12-14:00,2017-07-12-14:00 \
 # --create-video \
