f285707bab2e1db9c1d50babd9f97f434a3da796
Johannes Meuer <jomeuer@googlemail.com>
2021-12-10 11:27:36 +0100

    change eval script

--- start-scripts/start-evaluation-levante.sh
+++ start-scripts/start-evaluation-levante.sh
@@ -13,15 +13,15 @@ module source start-scripts/setup-modules.txt
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate.py \
- --device cuda --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
- --img-names radolan.h5 --mask-names single_radar_fail.h5 \
+ --device cuda --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr,tas \
+ --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/radolan-simple-2007-2013/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-simple-2007-2013/ \
+ --snapshot-dir snapshots/precipitation/radolan-rea2-celsius/ckpt/200000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-rea2-celsius/ \
  --lstm-steps 0 \
  --partitions 1177 \
- --eval-names Simple \
+ --eval-names Celsius \
  --infill test \
  --create-report \
 # --create-images 2017-07-12-14:00,2017-07-12-14:00 \
