cb2b589d2469d5bb2b7cdbfdca74e9a76b1ec279
Johannes Meuer <johannes.meuer@isc-events.com>
2021-12-13 12:35:29 +0100

    change start scripts

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -15,17 +15,17 @@ module load cdo
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate.py \
- --device cuda --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr,tas \
- --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
+ --device cuda --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
+ --img-names radolan.h5 --mask-names single_radar_fail.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/radolan-rea2-celsius/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-rea2-celsius/ \
- --lstm-steps 0 \
+ --snapshot-dir snapshots/precipitation/radolan-lstm-2007-2013/ckpt/200000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-lstm-2007-2013/ \
+ --lstm-steps 3 \
  --partitions 1177 \
- --eval-names Celsius \
+ --eval-names LSTM \
  --out-channels 1 \
- --create-report \
-# --infill test \
+ --infill test \
+# --create-report \
 # --create-images 2017-07-12-14:00,2017-07-12-14:00 \
 # --create-video \
