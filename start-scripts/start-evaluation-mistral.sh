ebc1de9cdd6e71bae64545957d9c839c10819013
Johannes Meuer <jomeuer@googlemail.com>
2021-12-14 10:32:15 +0100

    change start scripts

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -20,10 +20,10 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
  --snapshot-dir snapshots/precipitation/radolan-lstm-2007-2013/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-simple-2007-2013/,evaluation/precipitation/radolan-rea2-tas/,evaluation/precipitation/radolan-rea2-celsius/,evaluation/precipitation/radolan-lstm-2007-2013/ \
+ --evaluation-dirs evaluation/precipitation/radolan-simple-2007-2013/,evaluation/precipitation/radolan-rea2-tas/,evaluation/precipitation/radolan-rea2-celsius/,evaluation/precipitation/radolan-lstm-2007-2013/,evaluation/precipitation/radolan-prev-next-2007-2013/ \
  --lstm-steps 3 \
  --partitions 1177 \
- --eval-names Simple,Rea2-Kelvin,Re2-Celsius,LSTM \
+ --eval-names Simple,Rea2-Kelvin,Re2-Celsius,LSTM,Prev-Next \
  --out-channels 1 \
  --create-report \
 # --infill test \
