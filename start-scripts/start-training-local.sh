b295312d6311436965f6c1d82f7213277eb6af9b
Johannes Meuer <johannes.meuer@isc-events.com>
2021-12-15 12:20:34 +0100

    change start scripts

--- start-scripts/start-training-local.sh
+++ start-scripts/start-training-local.sh
@@ -2,14 +2,14 @@
 
 python /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
  --device cpu --batch-size 1 --image-size 72 --pooling-layers 0 --encoding-layers 3 --data-types tas,tas \
- --img-names 20cr-train.h5,20cr-train.h5 --mask-names single_temp_mask.h5,hadcrut4-missmask.h5 \
+ --img-names 20cr-train.h5,20cr-train.h5 --mask-names single_temp_mask.h5,single_temp_mask.h5 \
  --data-root-dir /home/joe/PycharmProjects/climatereconstructionAI/data/20cr/ \
  --mask-dir /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/masks/ \
  --snapshot-dir /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/snapshots/temperature/20cr-lstm-test1/ \
  --log-dir /home/joe/PycharmProjects/climatereconstructionAI/climatereconstructionAI/logs/temperature/20cr-lstm/ \
  --lstm-steps 0 \
- --prev-next-steps 2 \
- --out-channels 2 \
+ --prev-next-steps 1 \
+ --out-channels 1 \
  --max-iter 1000000 \
  --save-model-interval 10000 \
  --log-interval 10
