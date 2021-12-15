37f3c92745f5c0b9c3741189f9dc815a6ce6c4cb
Johannes Meuer <johannes.meuer@isc-events.com>
2021-12-15 17:01:09 +0100

    change start script

--- start-scripts/start-training-levante.sh
+++ start-scripts/start-training-levante.sh
@@ -11,22 +11,23 @@
 
 module source start-scripts/setup-modules.txt
 
+#singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
+# python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
+# --device cuda --batch-size 4 --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr,tas \
+# --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
+# --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
+# --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
+# --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-prev-next-rea/ \
+# --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-prev-next-rea/ \
+# --out-channels 1 \
+# --lstm-steps 0 \
+# --prev-next-steps 1 \
+# --max-iter 100000 \
+# --log-interval 100 \
+# --eval-timesteps 2143,2144,2145,2146,2147 \
+# --save-model-interval 10000
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
- --device cuda --batch-size 4 --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr,tas \
- --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
- --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
- --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
- --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-prev-next-rea/ \
- --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-prev-next-rea/ \
- --out-channels 1 \
- --lstm-steps 0 \
- --prev-next-steps 1 \
- --max-iter 100000 \
- --log-interval 100 \
- --eval-timesteps 2143,2144,2145,2146,2147 \
- --save-model-interval 10000
-singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  --device cuda --batch-size 4 --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr,tas \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
