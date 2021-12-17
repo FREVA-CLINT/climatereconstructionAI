c19dd358e4571353089c20fbd645714b2a75667a
Johannes Meuer <johannes.meuer@isc-events.com>
2021-12-17 16:03:36 +0100

    change start eval script

--- start-scripts/start-evaluation-levante.sh
+++ start-scripts/start-evaluation-levante.sh
@@ -13,12 +13,12 @@ module source start-scripts/setup-modules.txt
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate.py \
- --device cuda --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
- --img-names radolan.h5 --mask-names single_radar_fail.h5 \
+ --device cuda --image-size 512 --pooling-layers 3 --encoding-layers 4 --data-types pr,tas \
+ --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/radolan-prev-next-2007-2013/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-prev-next-2007-2013/ \
+ --snapshot-dir snapshots/precipitation/radolan-prev-next-rea/ckpt/200000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-prev-next-rea/ \
  --lstm-steps 0 \
  --prev-next-steps 1 \
  --partitions 1177 \
