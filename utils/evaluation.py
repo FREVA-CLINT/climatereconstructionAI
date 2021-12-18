e6c40192f52bdcc546bb870c7cc09c03382c43ec
Johannes Meuer <johannes.meuer@isc-group.com>
2021-12-18 20:33:27 +0100

    reconfigure lstm

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -27,10 +27,10 @@ def create_snapshot_image(model, dataset, filename, lstm_steps):
         output = model(image.to(cfg.device), mask.to(cfg.device)).to(cfg.device)
 
     # select last element of lstm sequence as evaluation element
-    image = image[:, lstm_steps, cfg.gt_channels, :, :].to(torch.device('cpu'))
-    gt = gt[:, lstm_steps, cfg.gt_channels, :, :].to(torch.device('cpu'))
-    mask = mask[:, lstm_steps, cfg.gt_channels, :, :].to(torch.device('cpu'))
-    output = output[:, 0, :, :, :].to(torch.device('cpu'))
+    image = image[:, 0, cfg.gt_channels, :, :].to(torch.device('cpu'))
+    gt = gt[:, 0, cfg.gt_channels, :, :].to(torch.device('cpu'))
+    mask = mask[:, 0, cfg.gt_channels, :, :].to(torch.device('cpu'))
+    output = output[:, lstm_steps, :, :, :].to(torch.device('cpu'))
 
     output_comp = mask * image + (1 - mask) * output
 
