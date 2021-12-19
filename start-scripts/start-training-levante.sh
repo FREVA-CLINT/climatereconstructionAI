16bcdd70faf8185a88c51677086148f1de07e50e
Johannes Meuer <johannes.meuer@isc-group.com>
2021-12-19 10:22:35 +0100

    change start script

--- start-scripts/start-training-levante.sh
+++ start-scripts/start-training-levante.sh
@@ -23,6 +23,7 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --lstm-steps 3 \
  --prev-next-steps 0 \
  --max-iter 100000 \
+ --resume-iter 50000 \
  --log-interval 100 \
  --eval-timesteps 2143,2144,2145,2146,2147 \
  --save-model-interval 10000
