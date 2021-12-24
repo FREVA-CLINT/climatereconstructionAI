4c85db41b52e62a5b5ac552b8cfe0dc77cbac8ac
Johannes Meuer <jomeuer@googlemail.com>
2021-12-24 12:19:43 +0100

    change fusion layer

--- start-scripts/start-training-local.sh
+++ start-scripts/start-training-local.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 python train_and_evaluate/train.py \
- --device cpu --batch-size 1 --image-sizes 72 --pooling-layers 1 --encoding-layers 1 --data-types tas \
- --img-names 20cr-train.h5 --mask-names single_temp_mask.h5 \
+ --device cpu --batch-size 1 --image-sizes 72,72 --pooling-layers 1,1 --encoding-layers 1,1 --data-types tas,tas \
+ --img-names 20cr-train.h5,20cr-train.h5 --mask-names single_temp_mask.h5,single_temp_mask.h5 \
  --data-root-dir ../data/20cr/ \
  --mask-dir masks/ \
  --snapshot-dir snapshots/temperature/20cr-lstm-test1/ \
  --log-dir logs/temperature/20cr-lstm/ \
- --lstm-steps 3 \
+ --lstm-steps 0 \
  --prev-next-steps 0 \
  --out-channels 1 \
  --max-iter 1000000 \
