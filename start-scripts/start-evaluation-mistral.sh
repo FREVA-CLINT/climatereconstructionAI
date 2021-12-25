6528d83956b4f7a0380d0d900d85fa84ec477972
Johannes Meuer <jomeuer@googlemail.com>
2021-12-25 13:37:27 +0100

    change evaluation lstm

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=64
 #SBATCH --time=12:00:00
 #SBATCH --mem=256G
-#SBATCH --nodelist=mg207
+#SBATCH --nodelist=mg206
 
 module load cuda/10.0.130
 module load singularity/3.6.1-gcc-9.1.0
@@ -15,17 +15,17 @@ module load cdo
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate.py \
- --device cuda --image-sizes 512,512 --pooling-layers 3,2 --encoding-layers 4,4 --data-types pr,tas \
- --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
+ --device cuda --image-sizes 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
+ --img-names radolan.h5 --mask-names single_radar_fail.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/radolan-fusion/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-simple-2007-2013/,evaluation/precipitation/radolan-rea2-tas/,evaluation/precipitation/radolan-rea2-celsius/,evaluation/precipitation/radolan-rea2-fusion/,evaluation/precipitation/radolan-fusion/ \
- --lstm-steps 0 \
+ --snapshot-dir snapshots/precipitation/radolan-autoenc2/ckpt/200000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-autoenc2/ \
+ --lstm-steps 3 \
  --partitions 1177 \
  --eval-names Simple,Kelvin,Celsius,Fusion,Fusion++ \
  --out-channels 1 \
- --create-report \
-# --infill test \
+ --infill test \
+# --create-report \
 # --create-images 2017-07-12-14:00,2017-07-12-14:00 \
 # --create-video \
