c0de4c9ab9faf7286cad1c9423f8aac2d4637bec
Johannes Meuer <jomeuer@googlemail.com>
2021-12-27 00:53:57 +0100

    changes start script

--- start-scripts/start-evaluation-levante.sh
+++ start-scripts/start-evaluation-levante.sh
@@ -13,16 +13,16 @@ module source start-scripts/setup-modules.txt
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate.py \
- --device cuda --image-sizes 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
- --img-names radolan.h5 --mask-names single_radar_fail.h5 \
+ --device cuda --image-sizes 512,512 --pooling-layers 3,2 --encoding-layers 4,4 --data-types pr,tas \
+ --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/radolan-autoenc2/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-autoenc2/ \
+ --snapshot-dir snapshots/precipitation/radolan-fusion3/ckpt/200000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-fusion3/ \
  --lstm-steps 0 \
- --prev-next-steps 1 \
+ --prev-next-steps 0 \
  --partitions 1177 \
- --eval-names Autoenc2 \
+ --eval-names Fusion3 \
  --infill test \
  --out-channels 1 \
  --create-report \
