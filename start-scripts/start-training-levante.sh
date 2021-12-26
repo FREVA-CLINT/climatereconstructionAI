59d26acab926982d2cad00fa6e47fcd47d99c30c
Johannes Meuer <jomeuer@googlemail.com>
2021-12-26 12:27:35 +0100

    change start script

--- start-scripts/start-training-levante.sh
+++ start-scripts/start-training-levante.sh
@@ -17,7 +17,7 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
  --img-names radolan.h5 --mask-names single_radar_fail.h5 \
- --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-prev-next-lstm>/ \
+ --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-prev-next-lstm/ \
  --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-prev-next-lstm/ \
  --out-channels 1 \
  --lstm-steps 3 \
@@ -34,11 +34,11 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --img-names radolan.h5 --mask-names single_radar_fail.h5 \
  --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-prev-next-lstm/ \
  --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-prev-next-lstm/ \
- --lstm-steps 0 \
- --prev-next-steps 3 \
+ --lstm-steps 3 \
+ --prev-next-steps 0 \
  --out-channels 1 \
  --max-iter 200000 \
- --resume-iter 120000 \
+ --resume-iter 100000 \
  --finetune \
  --log-interval 100 \
  --eval-timesteps 2143,2144,2145,2146,2147 \
