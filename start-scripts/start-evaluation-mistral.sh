1c21cc58973bc9991be2ddfc1f801ea2f07a503a
Johannes Meuer <johannes.meuer@isc-group.com>
2021-12-27 01:19:40 +0100

    change start script

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -15,15 +15,16 @@ module load cdo
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate.py \
- --device cuda --image-sizes 512,512 --pooling-layers 3,3 --encoding-layers 4,4 --data-types pr,tas \
+ --device cuda --image-sizes 512,512 --pooling-layers 3,2 --encoding-layers 4,4 --data-types pr,tas \
  --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/radolan-fuision2/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-fuision2/ \
+ --snapshot-dir snapshots/precipitation/radolan-fusion3/ckpt/200000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-fusion3/ \
  --lstm-steps 0 \
+ --prev-next-steps 0 \
  --partitions 1177 \
- --eval-names Simple,Kelvin,Celsius,Fusion,Fusion++ \
+ --eval-names Fusion3 \
  --out-channels 1 \
  --create-report \
 # --infill test \
