5d6c78e652dd95a682eb3eabb0b930e52f5075af
Johannes Meuer <jomeuer@googlemail.com>
2021-12-27 20:56:01 +0100

    change start script

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -19,12 +19,12 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/radolan-fusion3/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-fusion3/ \
+ --snapshot-dir snapshots/precipitation/radolan-fusion4/ckpt/200000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-fusion4/ \
  --lstm-steps 0 \
  --prev-next-steps 0 \
  --partitions 1177 \
- --eval-names Fusion3 \
+ --eval-names Fusion4 \
  --out-channels 1 \
  --create-report \
 # --infill test \
