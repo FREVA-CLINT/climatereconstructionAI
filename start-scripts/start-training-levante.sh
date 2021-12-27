39f0527b71a2533ba81298ed671c94a1e2ef6fb7
Johannes Meuer <jomeuer@googlemail.com>
2021-12-27 11:45:32 +0100

    add upsampling to fusion

--- start-scripts/start-training-levante.sh
+++ start-scripts/start-training-levante.sh
@@ -11,21 +11,21 @@
 
 module source start-scripts/setup-modules.txt
 
-singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
- python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
- --device cuda --batch-size 4 --image-sizes 512,512 --pooling-layers 3,3 --encoding-layers 4,4 --data-types pr,tas \
- --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
- --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
- --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
- --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-fusion4/ \
- --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-fusion4/ \
- --out-channels 1 \
- --lstm-steps 0 \
- --prev-next-steps 0 \
- --max-iter 100000 \
- --log-interval 100 \
- --eval-timesteps 2143,2144,2145,2146,2147 \
- --save-model-interval 5000
+#singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
+# python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
+# --device cuda --batch-size 4 --image-sizes 512,512 --pooling-layers 3,3 --encoding-layers 4,4 --data-types pr,tas \
+# --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
+# --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
+# --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
+# --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-fusion4/ \
+# --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-fusion4/ \
+# --out-channels 1 \
+# --lstm-steps 0 \
+# --prev-next-steps 0 \
+# --max-iter 100000 \
+# --log-interval 100 \
+# --eval-timesteps 2143,2144,2145,2146,2147 \
+# --save-model-interval 5000
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
  --device cuda --batch-size 4 --image-sizes 512,512 --pooling-layers 3,3 --encoding-layers 4,4 --data-types pr,tas \
