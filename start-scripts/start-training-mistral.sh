76029a8bdce28d315ac2a5e8cf0a59bf06cf772a
Johannes Meuer <johannes.meuer@isc-group.com>
2021-12-29 16:44:57 +0100

    change start script

--- start-scripts/start-training-mistral.sh
+++ start-scripts/start-training-mistral.sh
@@ -12,20 +12,20 @@
 module load cuda/10.0.130
 module load singularity/3.6.1-gcc-9.1.0
 
-singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
- python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
- --device cuda --batch-size 4 --image-sizes 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
- --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
- --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
- --img-names radolan.h5 --mask-names single_radar_fail.h5 \
- --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-simple-loss/ \
- --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-simple-loss/ \
- --out-channels 1 \
- --lstm-steps 0 \
- --prev-next-steps 0 \
- --max-iter 100000 \
- --eval-timesteps 2143,2144,2145,2146,2147 \
- --save-model-interval 10000
+#singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
+# python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
+# --device cuda --batch-size 4 --image-sizes 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
+# --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
+# --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
+# --img-names radolan.h5 --mask-names single_radar_fail.h5 \
+# --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-simple-loss/ \
+# --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-simple-loss/ \
+# --out-channels 1 \
+# --lstm-steps 0 \
+# --prev-next-steps 0 \
+# --max-iter 100000 \
+# --eval-timesteps 2143,2144,2145,2146,2147 \
+# --save-model-interval 10000
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
  --device cuda --batch-size 4 --image-sizes 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
@@ -38,7 +38,7 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --prev-next-steps 0 \
  --out-channels 1 \
  --max-iter 200000 \
- --resume-iter 120000 \
+ --resume-iter 100000 \
  --finetune \
  --eval-timesteps 2143,2144,2145,2146,2147 \
  --save-model-interval 10000
