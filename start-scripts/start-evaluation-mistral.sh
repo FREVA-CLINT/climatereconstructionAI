767da8d34f57733a4b94a6ea5bdaf2590296f309
Johannes Meuer <johannes.meuer@isc-events.com>
2022-01-03 14:51:51 +0100

    change start script

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -19,8 +19,8 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/radolan-attention/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-attention3/ \
+ --snapshot-dir snapshots/precipitation/radolan-attention/ckpt/190000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-attention4/ \
  --lstm-steps 0 \
  --prev-next-steps 0 \
  --partitions 1177 \
