68d4261c4f879a6ea33bdfecb8c553fcd8c3b747
Johannes Meuer <jomeuer@googlemail.com>
2022-01-04 15:25:23 +0100

    changes start script

--- start-scripts/start-training-local.sh
+++ start-scripts/start-training-local.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 python train_and_evaluate/train.py \
- --device cpu --batch-size 1 --image-sizes 72 --pooling-layers 0 --encoding-layers 3 --data-types tas \
+ --device cpu --batch-size 18 --image-sizes 72 --pooling-layers 0 --encoding-layers 3 --data-types tas \
  --img-names 20cr-train.h5 --mask-names single_temp_mask.h5 \
  --data-root-dir ../data/20cr/ \
  --mask-dir masks/ \
  --snapshot-dir snapshots/temperature/20cr-lstm-test1/ \
- --log-dir logs/temperature/20cr-lstm/ \
+ --log-dir logs/temperature/20cr-lstm-2/ \
  --lstm-steps 0 \
  --prev-next-steps 0 \
  --out-channels 1 \
