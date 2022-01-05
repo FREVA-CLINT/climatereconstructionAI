034b976acbd2ae8a4ca145ca9b9c380492d23d39
Johannes Meuer <johannes.meuer@isc-events.com>
2022-01-05 16:33:12 +0100

    change start script

--- start-scripts/start-training-levante.sh
+++ start-scripts/start-training-levante.sh
@@ -13,10 +13,10 @@ module source start-scripts/setup-modules.txt
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
- --device cuda --batch-size 4 --image-sizes 512,256,256 --pooling-layers 3,2,2 --encoding-layers 4,4,4 --data-types pr,uas,vas \
+ --device cuda --batch-size 4 --image-sizes 512,256 --pooling-layers 3,2 --encoding-layers 4,4 --data-types pr,uas \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
- --img-names radolan.h5,rea2-uas.h5,rea2-vas.h5 --mask-names single_radar_fail.h5,mask_ones_uas.h5,mask_ones_vas.h5 \
+ --img-names radolan.h5,rea2-uas.h5 --mask-names single_radar_fail.h5,mask_ones_uas.h5 \
  --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-rea-attention-hole/ \
  --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-rea-attention-hole/ \
  --out-channels 1 \
@@ -29,7 +29,7 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --save-snapshot-image
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
- --device cuda --batch-size 4 --image-sizes 512,256,256 --pooling-layers 3,2,2 --encoding-layers 4,4,4 --data-types pr,uas,vas \
+ --device cuda --batch-size 4 --image-sizes 512,256 --pooling-layers 3,2 --encoding-layers 4,4 --data-types pr,uas \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
  --img-names radolan.h5,rea2-uas.h5,rea2-vas.h5 --mask-names single_radar_fail.h5,mask_ones_uas.h5,mask_ones_vas.h5 \
