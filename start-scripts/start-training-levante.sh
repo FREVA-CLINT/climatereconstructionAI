75d5ca50fffdb462d494942cd8e93a3432b85eb6
Johannes Meuer <jomeuer@googlemail.com>
2022-01-05 13:36:32 +0100

    generalize loss function

--- start-scripts/start-training-levante.sh
+++ start-scripts/start-training-levante.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=128
 #SBATCH --time=100:00:00
 #SBATCH --mem=128GB
-#SBATCH --nodelist=vader2
+#SBATCH --nodelist=vader3
 
 module source start-scripts/setup-modules.txt
 
@@ -17,29 +17,32 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
  --img-names radolan.h5 --mask-names single_radar_fail.h5 \
- --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-simple-hole/ \
- --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-simple-hole/ \
+ --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-lstm-hole/ \
+ --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-lstm-hole/ \
  --out-channels 1 \
- --lstm-steps 0 \
+ --lstm-steps 2 \
  --prev-next-steps 0 \
  --max-iter 100000 \
- --log-interval 1000 \
+ --log-interval 100 \
  --eval-timesteps 2143,2144,2145,2146,2147 \
- --save-model-interval 5000
+ --save-model-interval 5000 \
+ --loss-criterion 1
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
  --device cuda --batch-size 4 --image-sizes 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/masks/ \
  --img-names radolan.h5 --mask-names single_radar_fail.h5 \
- --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-simple-hole/ \
- --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-simple-hole/ \
- --lstm-steps 0 \
+ --snapshot-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/snapshots/precipitation/radolan-lstm-hole/ \
+ --log-dir /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/logs/precipitation/radolan-lstm-hole/ \
+ --lstm-steps 2 \
  --prev-next-steps 0 \
  --out-channels 1 \
  --max-iter 200000 \
  --resume-iter 100000 \
  --finetune \
- --log-interval 1000 \
+ --log-interval 100 \
  --eval-timesteps 2143,2144,2145,2146,2147 \
- --save-model-interval 5000
+ --save-model-interval 5000 \
+ --loss-criterion 1
+
