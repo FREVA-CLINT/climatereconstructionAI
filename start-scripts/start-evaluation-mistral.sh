fd7183418f52b9825cd034f616039fb1ad3fd550
Johannes Meuer <johannes.meuer@isc-events.com>
2022-01-05 10:46:55 +0100

    change start script

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -15,18 +15,18 @@ module load cdo
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate.py \
- --device cuda --image-sizes 512,512 --pooling-layers 3,3 --encoding-layers 4,4 --data-types pr,tas \
- --img-names radolan.h5,rea2-tas-celsius.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5 \
+ --device cuda --image-sizes 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
+ --img-names radolan.h5 --mask-names single_radar_fail.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/radolan-attention/ckpt/190000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-attention/,evaluation/precipitation/radolan-attention3/,evaluation/precipitation/radolan-attention4/ \
+ --snapshot-dir snapshots/precipitation/radolan-simple-hole1/ckpt/200000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-simple-hole1/ \
  --lstm-steps 0 \
  --prev-next-steps 0 \
  --partitions 1177 \
- --eval-names Attention200,Attention200,Attention190 \
+ --eval-names attention2 \
  --out-channels 1 \
- --create-report \
-# --infill test \
+ --infill test \
+# --create-report \
 # --create-images 2017-07-12-14:00,2017-07-12-14:00 \
 # --create-video \
