127f63b5da35c61c1cf3fc8ef408b994b0f070a2
Johannes Meuer <jomeuer@googlemail.com>
2022-01-05 10:55:21 +0100

    change start script

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -19,8 +19,8 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --img-names radolan.h5 --mask-names single_radar_fail.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/radolan-simple-hole2/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-simple-hole2/ \
+ --snapshot-dir snapshots/precipitation/radolan-simple-hole/ckpt/200000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-simple-hole/ \
  --lstm-steps 0 \
  --prev-next-steps 0 \
  --partitions 1177 \
