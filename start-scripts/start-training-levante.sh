c782bb6f4563d55837b7afb5293a20e03e4c7cf2
Johannes Meuer <johannes.meuer@isc-events.com>
2022-01-06 10:15:51 +0100

    change start script

--- start-scripts/start-training-levante.sh
+++ start-scripts/start-training-levante.sh
@@ -27,6 +27,7 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --eval-timesteps 2143,2144,2145,2146,2147 \
  --save-model-interval 5000 \
  --save-snapshot-image \
+ --resume-iter 45000 \
  --loss-criterion 1
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
