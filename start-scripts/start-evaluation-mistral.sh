0dde59219e45d239352369a166f97441bb646544
Johannes Meuer <jomeuer@googlemail.com>
2022-01-06 10:39:29 +0100

    change start script

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=64
 #SBATCH --time=12:00:00
 #SBATCH --mem=256G
-#SBATCH --nodelist=mg207
+#SBATCH --nodelist=mg206
 
 module load cuda/10.0.130
 module load singularity/3.6.1-gcc-9.1.0
@@ -15,18 +15,17 @@ module load cdo
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate.py \
- --device cuda --image-sizes 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
- --img-names radolan.h5 --mask-names single_radar_fail.h5 \
+ --device cuda --batch-size 4 --image-sizes 512,256,256,256 --pooling-layers 3,2,2,2 --encoding-layers 4,4,4,4 --data-types pr,tas,uas,vas \
+ --img-names radolan.h5,rea2-tas.h5,rea2-uas.h5,rea2-vas.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5,mask_ones_uas.h5,mask_ones_vas.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/radolan-simple-hole/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-simple-2007-2013/,evaluation/precipitation/radolan-simple-loss/,evaluation/precipitation/radolan-simple-hole/,evaluation/precipitation/radolan-simple-hole1/,evaluation/precipitation/radolan-simple-hole2/ \
- --lstm-steps 0 \
+ --snapshot-dir snapshots/precipitation/radolan-rea-attention/ckpt/200000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-rea-attention-hole
  --prev-next-steps 0 \
  --partitions 1177 \
- --eval-names Old,NoExtractor,HoleTV,Hole,HoleSmallLR \
+ --eval-names Rea-Hole \
  --out-channels 1 \
- --create-images 2143,2149 \
-# --create-report \
-# --infill test \
+ --create-report \
+ --infill test \
+# --create-images 2143,2149 \
 # --create-video \
