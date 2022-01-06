1d6199d2d8197ead0034a960a1242eedf9a62f8b
Johannes Meuer <jomeuer@googlemail.com>
2022-01-06 10:41:56 +0100

    change start script

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -20,7 +20,7 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
  --snapshot-dir snapshots/precipitation/radolan-rea-attention/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-rea-attention-hole
+ --evaluation-dirs evaluation/precipitation/radolan-rea-attention-hole \
  --prev-next-steps 0 \
  --partitions 1177 \
  --eval-names Rea-Hole \
