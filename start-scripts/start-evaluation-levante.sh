8689750ca29c1b105f512132845c34e9f64226cc
Johannes Meuer <johannes.meuer@isc-events.com>
2022-01-06 15:34:17 +0100

    change start script

--- start-scripts/start-evaluation-levante.sh
+++ start-scripts/start-evaluation-levante.sh
@@ -13,18 +13,17 @@ module source start-scripts/setup-modules.txt
 
 singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/evaluate.py \
- --device cuda --image-sizes 512 --pooling-layers 3 --encoding-layers 4 --data-types pr \
- --img-names radolan.h5 --mask-names single_radar_fail.h5 \
+ --device cuda --image-sizes 512,256,256,256 --pooling-layers 3,2,2,2 --encoding-layers 4,4,4,4 --data-types pr,tas,uas,vas \
+ --img-names radolan.h5,rea2-tas.h5,rea2-uas.h5,rea2-vas.h5 --mask-names single_radar_fail.h5,mask_ones_tas.h5,mask_ones_uas.h5,mask_ones_vas.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/radolan-simple-hole/ckpt/200000.pth \
- --evaluation-dirs evaluation/precipitation/radolan-simple-hole/ \
- --lstm-steps 0 \
+ --snapshot-dir snapshots/precipitation/radolan-rea-attention/ckpt/150000.pth \
+ --evaluation-dirs evaluation/precipitation/radolan-rea-attention/ \
  --prev-next-steps 0 \
  --partitions 1177 \
- --eval-names attention2 \
+ --eval-names Rea \
  --out-channels 1 \
+ --create-report \
  --infill test \
-# --create-report \
-# --create-images 2017-07-12-14:00,2017-07-12-14:00 \
-# --create-video \
+ --create-images 2143,2149 \
+ --create-video
