278b55dc0734a0ebb4918cd3d9d5fca43ecd9c9f
Johannes Meuer <jomeuer@googlemail.com>
2022-01-07 23:55:27 +0100

    change start script

--- start-scripts/start-training-levante.sh
+++ start-scripts/start-training-levante.sh
@@ -26,7 +26,7 @@ python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/trai
  --eval-timesteps 2143,2144,2145,2146,2147 \
  --save-model-interval 5000 \
  --log-interval 100
-singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_mistral.sif \
+singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatereconstructionAI/torch_img_levante.sif \
  python /work/bb1152/k204233/climatereconstructionAI/climatereconstructionAI/train_and_evaluate/train.py \
  --device cuda --batch-size 4 --image-sizes 128 --pooling-layers 3 --encoding-layers 4 --data-types pr \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2-128x128/ \
@@ -38,7 +38,7 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --prev-next-steps 0 \
  --out-channels 1 \
  --max-iter 500000 \
- --resume-iter 200000 \
+ --resume-iter 250000 \
  --finetune \
  --eval-timesteps 2143,2144,2145,2146,2147 \
  --save-model-interval 5000 \
