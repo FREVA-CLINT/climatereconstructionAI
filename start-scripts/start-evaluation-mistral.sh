15460b232d6b6bd13767093efd030d594e80ce0b
Johannes Meuer <johannes.meuer@isc-events.com>
2022-01-09 18:08:57 +0100

    change start script

--- start-scripts/start-evaluation-mistral.sh
+++ start-scripts/start-evaluation-mistral.sh
@@ -19,14 +19,14 @@ singularity run --bind /work/bb1152/k204233/ --nv /work/bb1152/k204233/climatere
  --img-names radolan.h5 --mask-names single_radar_fail_128x128.h5 \
  --data-root-dir /work/bb1152/k204233/climatereconstructionAI/data/radolan-rea2-128x128/ \
  --mask-dir masks/ \
- --snapshot-dir snapshots/precipitation/128x128/2007-2013/lstm-prev-next-hole/ckpt/500000.pth \
- --evaluation-dirs evaluation/precipitation/128x128/2007-2011/lstm-prev-next-hole/ \
+ --snapshot-dir snapshots/precipitation/128x128/2007-2013/lstm-prev-next/ckpt/500000.pth \
+ --evaluation-dirs evaluation/precipitation/128x128/2007-2011/simple/,evaluation/precipitation/128x128/2007-2011/lstm-prev-next/,evaluation/precipitation/128x128/2007-2011/lstm-prev-next-hole/ \
  --prev-next-steps 0 \
  --lstm-steps 2 \
  --partitions 1177 \
  --eval-names Simple,LSTM,LSTM-hole \
  --out-channels 1 \
  --create-images 2140,2160 \
- --infill test \
-# --create-report \
-# --create-video \
+ --create-report \
+ --create-video \
+# --infill test \
