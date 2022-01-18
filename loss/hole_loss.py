d557e94a3d831bf8337c951e3f951c8a4386c695
Johannes Meuer <jomeuer@googlemail.com>
2022-01-18 21:56:07 +0100

    restructure model and loss

---
+++ loss/hole_loss.py
@@ -0,0 +1,24 @@
+import torch
+from torch import nn
+
+
+class HoleLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.l1 = nn.L1Loss()
+
+    def forward(self, mask, output, gt):
+        loss_dict = {
+            'hole': 0.0
+        }
+
+        # calculate loss for all channels
+        for channel in range(output.shape[1]):
+            # only select first channel
+            mask_ch = torch.unsqueeze(mask[:, channel, :, :], dim=1)
+            gt_ch = torch.unsqueeze(gt[:, channel, :, :], dim=1)
+            output_ch = torch.unsqueeze(output[:, channel, :, :], dim=1)
+
+            # define different loss functions from output and output_comp
+            loss_dict['hole'] += self.l1((1 - mask_ch) * output_ch, (1 - mask_ch) * gt_ch)
+        return loss_dict
---
+++ loss/utils.py
@@ -0,0 +1,15 @@
+import torch
+
+
+def gram_matrix(feat):
+    (b, ch, h, w) = feat.size()
+    feat = feat.view(b, ch, h * w)
+    feat_t = feat.transpose(1, 2)
+    gram = torch.bmm(feat, feat_t) / (ch * h * w)
+    return gram
+
+
+def total_variation_loss(image):
+    loss = torch.mean(torch.abs(image[:, :, :, :-1] - image[:, :, :, 1:])) + \
+           torch.mean(torch.abs(image[:, :, :-1, :] - image[:, :, 1:, :]))
+    return loss
