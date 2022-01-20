360aef16ffaaafd0563edf74f20999e34abff629
Johannes Meuer <johannes.meuer@isc-events.com>
2022-01-20 17:48:05 +0100

    combine lstm and attention module, final implementation

--- model/net.py
+++ model/net.py
@@ -5,6 +5,7 @@ import sys
 sys.path.append('./')
 from model.attention_module import AttentionEncoderBlock
 from model.encoder_decoder import EncoderBlock, DecoderBlock
+from model.conv_configs import init_enc_conv_configs, init_dec_conv_configs
 import config as cfg
 
 
@@ -16,108 +17,75 @@ class PConvLSTM(nn.Module):
         super().__init__()
 
         self.freeze_enc_bn = False
-        self.radar_enc_dec_layers = radar_enc_dec_layers
-        self.radar_pool_layers = radar_pool_layers
         self.net_depth = radar_enc_dec_layers + radar_pool_layers
         self.lstm = lstm
 
-        attention_enc_channels = [0] * self.net_depth
-        attention_dec_channels = [0] * self.net_depth
-        if rea_img_size:
-            self.rea_enc_layers = rea_enc_layers
-            self.rea_pool_layers = rea_pool_layers
-            self.attention_depth = rea_enc_layers + rea_pool_layers
-            if not cfg.attention:
-                for i in range(self.net_depth):
-                    if radar_img_size // (2 ** i) == rea_img_size:
-                        attention_enc_channels[i] = rea_in_channels
-                attention_dec_channels = attention_enc_channels
-            else:
-                attention_module_layers = []
-                for i in range(self.rea_enc_layers):
-                    if i == 0:
-                        in_channels = rea_in_channels
-                    else:
-                        in_channels = rea_img_size // (2 ** (self.rea_enc_layers - i))
-                    attention_module_layers.append(AttentionEncoderBlock(
-                        in_channels=in_channels,
-                        out_channels=rea_img_size // (2 ** (self.rea_enc_layers - i - 1)),
-                        image_size=rea_img_size // (2 ** i),
-                        kernel=(5, 5), stride=(2, 2), activation=nn.ReLU(), lstm=lstm))
-                    if i == self.attention_depth - 1:
-                        attention_dec_channels[self.net_depth - 1] = rea_img_size + rea_img_size // 2
-                    elif i != 0:
-                        attention_dec_channels[self.net_depth - (self.attention_depth - i) + (self.net_depth - self.attention_depth)] = rea_img_size // (2 ** (self.rea_enc_layers - i - 1))
+        # initialize channel inputs and outputs and image size for encoder and decoder
+        enc_conv_configs = init_enc_conv_configs(radar_img_size, radar_enc_dec_layers,
+                                                 radar_pool_layers, radar_in_channels)
+        dec_conv_configs = init_dec_conv_configs(radar_img_size, radar_enc_dec_layers,
+                                                 radar_pool_layers, radar_in_channels, radar_out_channels, 1)
 
-                # define encoding pooling layers
-                for i in range(self.rea_pool_layers):
-                    attention_module_layers.append(AttentionEncoderBlock(
-                        in_channels=rea_img_size,
-                        out_channels=rea_img_size,
-                        image_size=rea_img_size // (2 ** (self.rea_enc_layers + i)),
-                        kernel=(3, 3), stride=(2, 2), activation=nn.ReLU(), lstm=lstm))
-                    if i == self.attention_depth - self.rea_enc_layers - 1:
-                        attention_dec_channels[self.net_depth - 1] = rea_img_size + rea_img_size
-                    elif i != 0 or self.rea_enc_layers != 0:
-                        attention_dec_channels[self.net_depth - (self.attention_depth - (i + (self.net_depth - self.attention_depth)))] = rea_img_size // (
-                                    2 ** (self.rea_enc_layers - i - 1))
-                self.attention_module = nn.ModuleList(attention_module_layers)
+        if cfg.attention:
+            self.attention_depth = rea_enc_layers + rea_pool_layers
+            attention_enc_conv_configs = init_enc_conv_configs(rea_img_size, rea_enc_layers,
+                                                               rea_pool_layers, rea_in_channels)
+            attention_layers = []
+            for i in range(self.attention_depth):
+                if i < rea_enc_layers:
+                    kernel=(5, 5)
+                else:
+                    kernel=(3, 3)
+                attention_layers.append(AttentionEncoderBlock(
+                    conv_config=attention_enc_conv_configs[i],
+                    kernel=kernel, stride=(2, 2), activation=nn.ReLU(), lstm=lstm))
+
+                # adjust skip channels for decoder
+                if i != self.attention_depth - 1:
+                    dec_conv_configs[i]['out_channels'] += \
+                        attention_enc_conv_configs[self.attention_depth - i - 1]['in_channels']
+                dec_conv_configs[i]['skip_channels'] += \
+                    attention_enc_conv_configs[self.attention_depth - i - 1]['in_channels']
+                dec_conv_configs[i]['in_channels'] += attention_enc_conv_configs[self.attention_depth - i - 1][
+                    'out_channels']
+
+            # adjust in channels for first decoding layer
+
+            self.attention_module = nn.ModuleList(attention_layers)
+
+        elif rea_img_size:
+            self.channel_fusion_depth = rea_enc_layers + rea_pool_layers
+            enc_conv_configs[self.net_depth - self.channel_fusion_depth]['in_channels'] += rea_in_channels
+            dec_conv_configs[self.channel_fusion_depth - 1]['skip_channels'] += rea_in_channels
 
         # define encoding layers
         encoding_layers = []
-        encoding_layers.append(
-            EncoderBlock(
-                in_channels=radar_in_channels + attention_enc_channels[0],
-                out_channels=radar_img_size // (2 ** (self.radar_enc_dec_layers - 1)),
-                image_size=radar_img_size,
-                kernel=(7, 7), stride=(2, 2), activation=nn.ReLU(), lstm=lstm))
-        for i in range(1, self.radar_enc_dec_layers):
-            if i == self.radar_enc_dec_layers - 1:
-                encoding_layers.append(EncoderBlock(
-                    in_channels=radar_img_size // (2 ** (self.radar_enc_dec_layers - i)) + attention_enc_channels[i],
-                    out_channels=radar_img_size // (2 ** (self.radar_enc_dec_layers - i - 1)),
-                    image_size=radar_img_size // (2 ** i),
-                    kernel=(3, 3), stride=(2, 2), activation=nn.ReLU(), lstm=lstm))
+        for i in range(0, self.net_depth):
+            if i == 0:
+                kernel = (7, 7)
+            elif i < radar_enc_dec_layers - 1:
+                kernel = (5, 5)
             else:
-                encoding_layers.append(EncoderBlock(
-                    in_channels=radar_img_size // (2 ** (self.radar_enc_dec_layers - i)) + attention_enc_channels[i],
-                    out_channels=radar_img_size // (2 ** (self.radar_enc_dec_layers - i - 1)),
-                    image_size=radar_img_size // (2 ** i),
-                    kernel=(5, 5), stride=(2, 2), activation=nn.ReLU(), lstm=lstm))
-        # define encoding pooling layers
-        for i in range(self.radar_pool_layers):
+                kernel = (5, 5)
             encoding_layers.append(EncoderBlock(
-                in_channels=radar_img_size + attention_enc_channels[self.radar_enc_dec_layers + i],
-                out_channels=radar_img_size,
-                image_size=radar_img_size // (2 ** (self.radar_enc_dec_layers + i)),
-                kernel=(3, 3), stride=(2, 2), activation=nn.ReLU(), lstm=lstm))
+                conv_config=enc_conv_configs[i],
+                kernel=kernel, stride=(2, 2), activation=nn.ReLU(), lstm=lstm))
         self.encoder = nn.ModuleList(encoding_layers)
 
-        # define decoding pooling layers
-        decoding_layers = []
-        for i in range(self.radar_pool_layers):
-            decoding_layers.append(DecoderBlock(
-                in_channels=radar_img_size,
-                in_skip_channels=radar_img_size + attention_dec_channels[self.net_depth - i - 1],
-                out_channels=radar_img_size,
-                image_size=radar_img_size // (2 ** (self.net_depth - i - 1)),
-                kernel=(3, 3), stride=(1, 1), activation=nn.LeakyReLU(), lstm=lstm))
         # define decoding layers
-        for i in range(1, self.radar_enc_dec_layers):
-            decoding_layers.append(
-                DecoderBlock(
-                    in_channels=radar_img_size // (2 ** (i - 1)),
-                    in_skip_channels=radar_img_size // (2 ** i) + attention_dec_channels[self.net_depth - i],
-                    out_channels=radar_img_size // (2 ** i),
-                    image_size=radar_img_size // (2 ** (self.net_depth - self.radar_pool_layers - i)),
-                    kernel=(3, 3), stride=(1, 1), activation=nn.LeakyReLU(), lstm=lstm))
-        decoding_layers.append(
-            DecoderBlock(
-                in_channels=radar_img_size // (2 ** (self.radar_enc_dec_layers - 1)),
-                in_skip_channels=radar_in_channels + attention_dec_channels[0],
-                out_channels=radar_out_channels,
-                image_size=radar_img_size,
-                kernel=(3, 3), stride=(1, 1), activation=None, lstm=lstm, bn=False, bias=True))
+        decoding_layers = []
+        for i in range(self.net_depth):
+            if i == self.net_depth - 1:
+                activation = None
+                bn = False
+                bias = True
+            else:
+                activation = nn.LeakyReLU()
+                bn = True,
+                bias = False
+            decoding_layers.append(DecoderBlock(
+                conv_config=dec_conv_configs[i],
+                kernel=(3, 3), stride=(1, 1), activation=activation, lstm=lstm, bn=bn, bias=bias))
         self.decoder = nn.ModuleList(decoding_layers)
 
     def forward(self, input, input_mask, rea_input, rea_input_mask):
@@ -162,6 +130,8 @@ class PConvLSTM(nn.Module):
                 attentions_lstm_states.append(rea_lstm_state)
         # concat attentions
         if attentions:
+            hs[self.net_depth - self.attention_depth] = torch.cat([hs[self.net_depth - self.attention_depth], rea_input], dim=2)
+            hs_mask[self.net_depth - self.attention_depth] = torch.cat([hs_mask[self.net_depth - self.attention_depth], rea_input_mask], dim=2)
             for i in range(self.attention_depth):
                 hs[i + (self.net_depth - self.attention_depth) + 1] = torch.cat([hs[i + (self.net_depth - self.attention_depth) + 1], attentions[i]], dim=2)
                 hs_mask[i + (self.net_depth - self.attention_depth) + 1] = torch.cat([hs_mask[i + (self.net_depth - self.attention_depth) + 1], attentions_mask[i]], dim=2)
--- start-scripts/start-training-local.sh
+++ start-scripts/start-training-local.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 python train_and_evaluate/train.py \
- --device cpu --batch-size 2 --image-sizes 128,64,64 --pooling-layers 3,2,2 --encoding-layers 4,4,4 --data-types pr,tas,uas \
- --img-names radolan.h5,rea2-tas.h5,rea2-uas.h5 --mask-names single_radar_fail_128x128.h5,mask_ones_tas_64x64.h5,mask_ones_uas_64x64.h5 \
+ --device cpu --batch-size 2 --image-sizes 128,64,64,64 --pooling-layers 3,2,2,2 --encoding-layers 4,4,4,4 --data-types pr,tas,uas,vas \
+ --img-names radolan.h5,rea2-tas.h5,rea2-uas.h5,rea2-vas.h5 --mask-names single_radar_fail_128x128.h5,mask_ones_tas_64x64.h5,mask_ones_uas_64x64.h5,mask_ones_vas_64x64.h5 \
  --data-root-dir ../data/radolan-rea2-128x128/ \
  --mask-dir masks/ \
  --snapshot-dir snapshots/temperature/20cr-lstm-test1/ \
@@ -14,4 +14,4 @@ python train_and_evaluate/train.py \
  --save-model-interval 10000 \
  --log-interval 1000 \
  --save-snapshot-image \
-# --attention
+ --attention
