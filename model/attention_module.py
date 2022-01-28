80ce7de506645a8676090b4742b5fe5c79ae468c
Johannes Meuer <johannes.meuer@isc-events.com>
2022-01-28 16:00:55 +0100

    reset to attention3.0 again

--- model/attention_module.py
+++ model/attention_module.py
@@ -15,47 +15,43 @@ class AttentionEncoderBlock(nn.Module):
                                              activation=activation, lstm=lstm)
         self.channel_attention_block = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(in_features=conv_config['in_channels'],
-                      out_features=conv_config['in_channels'] // cfg.channel_reduction_rate),
+            nn.Linear(in_features=conv_config['out_channels'],
+                      out_features=conv_config['out_channels'] // cfg.channel_reduction_rate),
             nn.ReLU(),
-            nn.Linear(in_features=conv_config['in_channels'] // cfg.channel_reduction_rate,
-                      out_features=conv_config['in_channels']),
+            nn.Linear(in_features=conv_config['out_channels'] // cfg.channel_reduction_rate,
+                      out_features=conv_config['out_channels']),
         )
         self.spatial_attention_block = nn.Sequential(
             nn.Conv2d(in_channels=2, out_channels=1, kernel_size=(7, 7), padding=(3, 3)),
             nn.Sigmoid()
         )
 
-    def forward(self, h_rea, h_rea_mask, rea_lstm_state, h, h_mask):
+    def forward(self, h_rea, h_rea_mask, rea_lstm_state, h):
+        h_rea, h_rea_mask, rea_lstm_state = self.partial_conv_enc(h_rea, h_rea_mask, rea_lstm_state)
         batch_size = h_rea.shape[0]
 
         # convert lstm steps to batch dimension
         h_rea = lstm_to_batch(h_rea)
         h_rea_mask = lstm_to_batch(h_rea_mask)
         h = lstm_to_batch(h)
-        h_mask = lstm_to_batch(h_mask)
 
         # channel attention
-        channel_attention = self.forward_channel_attention(h_rea, h_rea_mask)
+        channel_attention = self.forward_channel_attention(h_rea)
         # spatial attention
         spatial_attention = self.spatial_attention_block(
             torch.cat([torch.max(h, keepdim=True, dim=1)[0],
                        torch.mean(h, keepdim=True, dim=1)], dim=1)
         )
         attention = channel_attention * spatial_attention
-        attention_mask = torch.ones_like(attention)
 
         # convert batches to lstm dimension
         h_rea = batch_to_lstm(h_rea, batch_size)
         h_rea_mask = batch_to_lstm(h_rea_mask, batch_size)
         attention = batch_to_lstm(attention, batch_size)
-        attention_mask = batch_to_lstm(attention_mask, batch_size)
 
-        h_rea, h_rea_mask, rea_lstm_state = self.partial_conv_enc(h_rea, h_rea_mask, rea_lstm_state)
+        return h_rea, h_rea_mask, rea_lstm_state, attention
 
-        return h_rea, h_rea_mask, rea_lstm_state, attention, attention_mask
-
-    def forward_channel_attention(self, input, input_mask):
+    def forward_channel_attention(self, input):
         attention_max = F.max_pool2d(input, input.shape[2])
         attention_avg = F.avg_pool2d(input, input.shape[2])
         total_attention = torch.sigmoid(
