e3605847a67a1048c6c4eb354a086488d949b2d2
Johannes Meuer <johannes.meuer@isc-group.com>
2022-03-01 13:29:47 +0100

    add maximum

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -475,9 +475,10 @@ def create_evaluation_maps(gt, outputs):
     map_lists = [timcor_maps, rmse_maps, sum_maps]
     map_names = [timcor_names, rmse_names, sum_names]
     for i in range(len(map_lists)):
+        maximum = np.max(map_lists[i])
         for j in range(len(map_lists[i])):
             # plot and save data
-            img = plt.imshow(np.squeeze(map_lists[i][j]), vmin=0, vmax=np.max(map_lists[i][j]), cmap='jet', aspect='auto')
+            img = plt.imshow(np.squeeze(map_lists[i][j]), vmin=0, vmax=maximum, cmap='jet', aspect='auto')
             plt.title(map_names[i][j])
             plt.xlabel("km")
             plt.ylabel("km")
