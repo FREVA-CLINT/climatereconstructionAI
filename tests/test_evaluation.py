9856a53c9b4cb0f2cc168bd4f721a159e08cfcb4
Étienne Plésiat <plesiat@dkrz.de>
2022-03-01 19:01:05 +0100

    Add first training and evaluation tests

---
+++ tests/test_evaluation.py
@@ -0,0 +1,15 @@
+import pytest
+
+testdir = "tests/"
+
+@pytest.mark.evaluation
+def test_evaluation_run():
+    from climatereconstructionai import evaluate
+    evaluate(testdir+"eval-1.inp")
+
+@pytest.mark.evaluation
+def test_comp_netcdf():
+    import xarray as xr
+    ds_ref = xr.open_dataset(testdir+"ref/test-1_output_comp.nc")
+    ds_run = xr.open_dataset(testdir+"out/evaluation/test-1_output_comp.nc")
+    assert ds_ref.equals(ds_run)
