23824e4da86d478e2d2ce1193d7c5fe64cb75a06
Étienne Plésiat <plesiat@dkrz.de>
2022-05-31 18:24:29 +0200

    Normalize images before applying feature extraction

--- climatereconstructionai/utils/featurizer.py
+++ climatereconstructionai/utils/featurizer.py
@@ -1,10 +1,12 @@
 from torch import nn
-from torchvision import models
-
+from torchvision import models, transforms
 
 class VGG16FeatureExtractor(nn.Module):
     def __init__(self):
         super().__init__()
+
+        self.img_tf = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
         vgg16 = models.vgg16(pretrained=True)
         self.enc_1 = nn.Sequential(*vgg16.features[:5])
         self.enc_2 = nn.Sequential(*vgg16.features[5:10])
@@ -16,7 +18,7 @@ class VGG16FeatureExtractor(nn.Module):
                 param.requires_grad = False
 
     def forward(self, image):
-        results = [image]
+        results = [self.img_tf(torch.cat([image] * 3, 1))]
         for i in range(3):
             func = getattr(self, 'enc_{:d}'.format(i + 1))
             results.append(func(results[-1]))
