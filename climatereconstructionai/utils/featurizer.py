f52bf0d5f9902bd367368fa51ca2034d37d9e847
Étienne Plésiat <plesiat@dkrz.de>
2022-06-01 11:21:57 +0200

    Revert to unnormalized feature extraction

--- climatereconstructionai/utils/featurizer.py
+++ climatereconstructionai/utils/featurizer.py
@@ -1,11 +1,10 @@
+import torch
 from torch import nn
-from torchvision import models, transforms
+from torchvision import models
 
 class VGG16FeatureExtractor(nn.Module):
     def __init__(self):
         super().__init__()
-
-        self.img_tf = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
         vgg16 = models.vgg16(pretrained=True)
         self.enc_1 = nn.Sequential(*vgg16.features[:5])
@@ -18,7 +17,7 @@ class VGG16FeatureExtractor(nn.Module):
                 param.requires_grad = False
 
     def forward(self, image):
-        results = [self.img_tf(torch.cat([image] * 3, 1))]
+        results = [torch.cat([image] * 3, 1)]
         for i in range(3):
             func = getattr(self, 'enc_{:d}'.format(i + 1))
             results.append(func(results[-1]))
