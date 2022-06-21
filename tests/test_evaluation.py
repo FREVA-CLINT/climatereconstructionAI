057a216e81ccfe44159e756f844b6ae92c57163a
Étienne Plésiat <plesiat@dkrz.de>
2022-06-21 18:06:18 +0200

    Update tests to new Pytorch version

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 
 testdir = "tests/"
 
@@ -12,4 +13,5 @@ def test_comp_netcdf():
     import xarray as xr
     ds_ref = xr.open_dataset(testdir+"ref/test-1_infilled.nc")
     ds_run = xr.open_dataset(testdir+"out/evaluation/test-1_infilled.nc")
-    assert ds_ref.equals(ds_run)
+    # assert ds_ref.equals(ds_run)
+    xr.testing.assert_allclose(ds_ref, ds_run, rtol=1e-15, atol=1e-8)
