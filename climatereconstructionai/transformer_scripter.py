57cba2d4fd8a77b363563053e78a079967db65a7
Maximilian Witte <max-witte@gmx.de>
2023-08-11 14:39:59 +0200

    added scripter for slurm

---
+++ climatereconstructionai/transformer_scripter.py
@@ -0,0 +1,22 @@
+import os
+import json
+import argparse
+from climatereconstructionai.model import EncDecSpatialTransformer
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f","--script_dict")
+
+    
+if __name__ == "__main__":
+    
+    args = parser.parse_args()
+    script_dict = args.script_dict
+
+    if isinstance(script_dict, str):
+        with open(script_dict,'r') as file:
+            script_dict = json.load(file)
+
+    model = EncDecSpatialTransformer.SpatialTransNet(script_dict['model_settings'])
+    model.train_(script_dict['training_settings']) 
+
