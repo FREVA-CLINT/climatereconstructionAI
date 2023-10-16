66da7f5e0a27a63a52bb3f9f496775c5fb2f760f
Maximilian Witte <max-witte@gmx.de>
2023-10-16 11:08:25 +0200

    missing from last commit

--- debug.py
+++ debug.py
@@ -1,4 +1,4 @@
-from climatereconstructionai.model import EncDecLGTransformer
+from climatereconstructionai.model import EncDecLGTransformer,trans_u_net
 
 from climatereconstructionai import config as cfg
 import json
@@ -9,4 +9,18 @@ if __name__ == "__main__":
   training_settings = '/Users/maxwitte/work/crai_sr/src/climatereconstructionAI_remote/climatereconstructionAI/notebooks/transformer_settings/training_settings.json'
 
   model = EncDecLGTransformer.SpatialTransNet(model_settings)
-  model.train_(training_settings)
+  model.train_(training_settings) 
+
+
+  #model_settings = '/Users/maxwitte/work/crai_sr/src/climatereconstructionAI_remote/climatereconstructionAI/notebooks/transformer_settings/LGEncDec_Transformer_infer.json'
+  #settings = '/Users/maxwitte/work/crai_sr/src/climatereconstructionAI_remote/climatereconstructionAI/notebooks/transformer_settings/inference_settings.json'
+
+  #model = EncDecLGTransformer.SpatialTransNet(model_settings)
+  #model.infer(settings) 
+
+
+  #model_settings = '/Users/maxwitte/work/crai_sr/src/climatereconstructionAI_remote/climatereconstructionAI/notebooks/transformer_settings/TransUNet.json'
+  #settings = '/Users/maxwitte/work/crai_sr/src/climatereconstructionAI_remote/climatereconstructionAI/notebooks/transformer_settings/training_settings_trans_unet.json'
+
+  #model = trans_u_net.TransUNet(model_settings)
+  #model.train(settings) 
