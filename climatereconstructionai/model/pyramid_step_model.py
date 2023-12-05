fixed normalization bug

climatereconstructionai/model/pyramid_step_model.py
@@ -140,7 +140,6 @@ class quant_discretizer(nn.Module):
     def __init__(self, min_val, max_val, n) -> None: 
         super().__init__()
 
-        self.n_c = 8
         self.min_nq = 4
 
         self.min_val = min_val
@@ -153,7 +152,7 @@ class quant_discretizer(nn.Module):
         n = x.shape[1]
         n_q = int((x.shape[1] // self.n_c)**0.5)
         n_q = self.min_nq if n_q < self.min_nq else n_q
-        f = (self.n // n_q)
+        f = (self.n // n_q) + 1
 
         coords = coords_source
         data = x

climatereconstructionai/utils/netcdfloader_samples.py
@@ -34,12 +34,12 @@ class InfiniteSampler(Sampler):
                 order = np.random.permutation(n_samples) 
                 i = 0
 
-def get_moments(data, type, level=0.05):
+def get_moments(data, type, level=0.9):
 
     if type == 'quantile':
-        moments = (np.quantile((data), 1-level).astype(float), np.quantile((data), level).astype(float))
+        moments = (np.quantile((data), ((1-level), level)).astype(float))
 
-    if type == 'quantile_abs':
+    elif type == 'quantile_abs':
         q = np.quantile(np.abs(data), level).astype(float)
         moments = (q,q)
 