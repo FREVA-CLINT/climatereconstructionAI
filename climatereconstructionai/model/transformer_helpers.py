mixed precision mask value bug fix

climatereconstructionai/model/transformer_helpers.py
@@ -65,7 +65,8 @@ def scaled_dot_product_rpe(q, k, v, aq=None, ak=None, av=None, bias=None, mask=N
 
     if mask is not None:
         mask = mask.reshape(attn_logits.shape[0],1,mask.shape[-2],mask.shape[-1]).repeat(1,attn_logits.shape[1],1,1)
-        attn_logits = attn_logits.masked_fill(mask, -1e10)
+        mask_value = -1e10 if attn_logits.dtype == torch.float32 else -1e4
+        attn_logits = attn_logits.masked_fill(mask, mask_value)
 
     #softmax and scale
     attention = F.softmax(attn_logits, dim=-1)