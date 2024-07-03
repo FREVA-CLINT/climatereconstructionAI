fixed distance calculation

climatereconstructionai/model/ICONTransformer_decomp.py
@@ -771,7 +771,7 @@ class ICON_Transformer(nn.Module):
         self.polar = True if "polar" in  self.pos_emb_calc else False
 
         self.grid = xr.open_dataset(self.model_settings['processing_grid'])
-        self.register_buffer('global_indices', torch.arange(len(self.grid.clon)).unsqueeze(dim=0), persistent=False)
+        
 
         n_grid_levels = self.model_settings['n_grid_levels']
 
@@ -786,6 +786,7 @@ class ICON_Transformer(nn.Module):
         else:
             mgrids = torch.load(self.model_settings['mgrids_path'])
 
+        self.register_buffer('global_indices', torch.arange(mgrids[0]['coords'].shape[1]).unsqueeze(dim=0), persistent=False)
         self.register_buffer('cell_coords_global', mgrids[0]['coords'], persistent=False)  
 
         self.input_data  = self.model_settings['variables_source']
@@ -1009,7 +1010,6 @@ class ICON_Transformer(nn.Module):
                                     self.model_settings['input_grid'], self.input_data, 
                                     search_raadius=self.model_settings['search_raadius'], 
                                     max_nh=self.model_settings['nh_input'], 
-                                    level_start=int(torch.min(torch.tensor([self.model_settings['level_start_input'], self.n_grid_levels_fov -1]))), 
                                     lowest_level=0,
                                     coords_icon=mgrid_0_coords)
 
@@ -1022,7 +1022,6 @@ class ICON_Transformer(nn.Module):
                                     self.model_settings['output_grid'], self.output_data, 
                                     search_raadius=self.model_settings['search_raadius'], 
                                     max_nh=1, 
-                                    level_start=int(torch.min(torch.tensor([self.model_settings['level_start_input'], self.n_grid_levels_fov -1]))),
                                     lowest_level=0,
                                     reverse_last=False,
                                     coords_icon=mgrid_0_coords)

climatereconstructionai/utils/grid_utils.py
@@ -915,7 +915,7 @@ def get_nearest_to_icon_rec(c_t_global, c_i, level=7, global_indices_i=None, nh=
 
     c_t_m = c_t_m.reshape(2, n_sec_i, -1)
 
-    dist, phi = get_distance_angle(c_t_m[0].unsqueeze(dim=-1),c_t_m[1].unsqueeze(dim=-1), c_i_[0].unsqueeze(dim=-2),c_i_[1].unsqueeze(dim=-2))
+    dist, phi = get_distance_angle(c_t_m[0].unsqueeze(dim=-1),c_t_m[1].unsqueeze(dim=-1), c_i_[0].unsqueeze(dim=-2), c_i_[1].unsqueeze(dim=-2))
     dist = dist.reshape(n_level, -1)
     phi = phi.reshape(n_level, -1)
 
@@ -930,7 +930,10 @@ def get_nearest_to_icon_rec(c_t_global, c_i, level=7, global_indices_i=None, nh=
         global_indices = torch.gather(global_indices_i, index=indices_rel.reshape(global_indices_i.shape[0],-1), dim=-1)
         global_indices = global_indices.reshape(n_level,-1)
     
-    n_keep = torch.tensor([nh, dist_values.shape[1]]).min()
+    if nh is not None:
+        n_keep = nh
+    else:
+        n_keep = dist_values.shape[1]
 
     indices_keep = dist_values.topk(int(n_keep), dim=-1, largest=False, sorted=True)[1]
 
@@ -943,7 +946,9 @@ def get_nearest_to_icon_rec(c_t_global, c_i, level=7, global_indices_i=None, nh=
     return global_indices, in_rad, (dist_values, phi_values)
 
 
-def get_mapping_to_icon_grid(coords_icon, coords_input, search_raadius=3, max_nh=10, level_start=7, lowest_level=0, reverse_last=False):
+def get_mapping_to_icon_grid(coords_icon, coords_input, search_raadius=3, max_nh=10, lowest_level=0, reverse_last=False):
+
+    level_start = int(math.log(coords_icon.shape[-1])/math.log(4))
 
     grid_mapping = []
     for k in range(level_start + 1 - lowest_level):
@@ -951,8 +956,10 @@ def get_mapping_to_icon_grid(coords_icon, coords_input, search_raadius=3, max_nh
 
         nh = max_nh*4**level
 
-        if k == level_start-lowest_level:
+        if level == lowest_level:
             nh = max_nh
+        else:
+            nh = None
 
         if k == 0:
             indices, in_rng, pos = get_nearest_to_icon_rec(coords_icon, coords_input.unsqueeze(dim=1), level=level, nh=nh, search_radius=search_raadius)
@@ -987,7 +994,7 @@ def get_mapping_to_icon_grid(coords_icon, coords_input, search_raadius=3, max_nh
     return grid_mapping
 
 
-def get_nh_variable_mapping_icon(grid_file_icon, grid_types_icon, grid_file, grid_types, search_raadius=3, max_nh=10, level_start=7, lowest_level = 0, return_last=True, reverse_last=False, coords_icon=None):
+def get_nh_variable_mapping_icon(grid_file_icon, grid_types_icon, grid_file, grid_types, search_raadius=3, max_nh=10, lowest_level = 0, return_last=True, reverse_last=False, coords_icon=None):
     
     grid_icon = xr.open_dataset(grid_file_icon)
     grid = xr.open_dataset(grid_file)
@@ -1027,7 +1034,6 @@ def get_nh_variable_mapping_icon(grid_file_icon, grid_types_icon, grid_file, gri
                     coords_input,
                     search_raadius=search_raadius,
                     max_nh=max_nh,
-                    level_start=level_start,
                     lowest_level = lowest_level,
                     reverse_last=reverse_last)
                 
@@ -1061,7 +1067,7 @@ def icon_grid_to_mgrid(grid, n_grid_levels, clon_fov=None, clat_fov=None, nh=0, 
 
     if clon_fov is not None or clat_fov is not None:
 
-        indices_max_lvl = torch.arange(len(grid.clon)).reshape(-1,4**int(n_grid_levels-1))
+        indices_max_lvl = torch.arange(len(grid.clon)).reshape(-1,4**int(n_grid_levels))
        
         clon_max_lvl = clon[indices_max_lvl[:,0]]
         clat_max_lvl = clat[indices_max_lvl[:,0]]

climatereconstructionai/utils/netcdfloader_trans.py
@@ -189,22 +189,18 @@ class NetCDFLoader_lazy(Dataset):
                 mgrids = icon_grid_to_mgrid(grid_processing, n_grid_levels_fov, clon_fov=clon_fov, clat_fov=clat_fov, nh=1, extension=0.1)
             else:
                 mgrids = torch.load(self.model_settings['mgrids_path'])
-            
-            level_start = np.min([n_grid_levels_fov-1, model_settings['level_start_input']])
 
             input_mapping, input_in_range = get_nh_variable_mapping_icon(model_settings['processing_grid'], ['cell'], 
                                                         model_settings['input_grid'], self.variables_source.keys(), 
                                                         search_raadius=model_settings['search_raadius'], 
-                                                        max_nh=model_settings['nh_input'], 
-                                                        level_start=level_start,
+                                                        max_nh=model_settings['nh_input'],
                                                         lowest_level=0,
                                                         coords_icon=mgrids[0]['coords'])
 
             output_mapping, output_in_range = get_nh_variable_mapping_icon(model_settings['processing_grid'], ['cell'], 
                                                         model_settings['output_grid'], self.variables_target.keys(), 
                                                         search_raadius=model_settings['search_raadius'], 
-                                                        max_nh=1, 
-                                                        level_start=level_start,
+                                                        max_nh=1,
                                                         lowest_level=0,
                                                         coords_icon=mgrids[0]['coords'])
                             