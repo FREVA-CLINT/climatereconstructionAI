patch scaling bug fix + added depth embedding

climatereconstructionai/model/core_model_resushuffle.py
@@ -19,6 +19,39 @@ def ICNR(tensor, initializer, upscale_factor=2, *args, **kwargs):
     return sub_kernel.repeat_interleave(upscale_factor_squared, dim=0)
 
 
+class DepthEncoding(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, depth):
+        count = self.dim // 2
+        step = torch.arange(count, dtype=depth.dtype,
+                            device=depth.device) / count
+        encoding = depth.unsqueeze(
+            1) * torch.exp(-math.log(1e4) * step.unsqueeze(0))
+        encoding = torch.cat([torch.sin(encoding), torch.cos(encoding)], dim=-1)
+        return encoding
+    
+
+class FeatureWiseAffine(nn.Module):
+    def __init__(self, in_channels, out_channels, with_scale=False):
+        super(FeatureWiseAffine, self).__init__()
+        self.with_scale = with_scale
+        self.noise_func = nn.Sequential(
+            nn.Linear(in_channels, out_channels*(1+self.with_scale))
+        )
+
+    def forward(self, x, noise_embed):
+        batch = x.shape[0]
+        if self.with_scale:
+            gamma, beta = self.noise_func(noise_embed).view(
+                batch, -1, 1, 1).chunk(2, dim=1)
+            x = (1 + gamma) * x + beta
+        else:
+            x = x + self.noise_func(noise_embed).view(batch, -1, 1, 1)
+        return x
+
 class shuffle_upscaling(nn.Module):
     def __init__(self, in_channels, upscale_factor, k_size=3, bias=False, global_padding=False):
         super().__init__()
@@ -46,7 +79,7 @@ class shuffle_upscaling(nn.Module):
         return x
 
 class res_net_block(nn.Module):
-    def __init__(self, hw, in_channels, out_channels, k_size=3, batch_norm=True, stride=1, groups=1, dropout=0, with_att=False, with_res=True, bias=True, out_activation=True, global_padding=False):
+    def __init__(self, hw, in_channels, out_channels, k_size=3, batch_norm=True, stride=1, groups=1, dropout=0, with_att=False, with_res=True, bias=True, out_activation=True, global_padding=False, depth_embedding_dim=0):
         super().__init__()
 
         self.global_padding = global_padding
@@ -61,6 +94,11 @@ class res_net_block(nn.Module):
 
         if with_res:
             self.reduction = nn.Conv2d(in_channels, out_channels, stride=stride, padding=0, kernel_size=1, groups=groups, bias=bias)
+
+        self.with_depth_embedding = True if depth_embedding_dim>0 else False
+
+        if self.with_depth_embedding:
+            self.feat_affine = FeatureWiseAffine(depth_embedding_dim, out_channels, with_scale=True)
 
         self.with_res = with_res
 
@@ -77,7 +115,7 @@ class res_net_block(nn.Module):
         if with_att:
             self.att = helpers.ConvSelfAttention(out_channels, hw, n_heads=1)
 
-    def forward(self, x):
+    def forward(self, x, depth_emb=None):
 
         if self.with_res:
             x_res = self.reduction(x)
@@ -89,6 +127,9 @@ class res_net_block(nn.Module):
         x = self.bn1(x)
         x = self.activation1(x)
         x = self.dropout1(x)
+
+        if self.with_depth_embedding:
+            x = self.feat_affine(x, depth_emb)
 
         if self.global_padding:
             x = global_pad(x, self.padding)
@@ -108,15 +149,15 @@ class res_net_block(nn.Module):
         
 
 class res_blocks(nn.Module): 
-    def __init__(self, hw, n_blocks, in_channels, out_channels, k_size=3, batch_norm=True, groups=1, dropout=0, with_att=False, with_res=True, out_activation=True, bias=True, global_padding=False, factor=2, down_method='max'):
+    def __init__(self, hw, n_blocks, in_channels, out_channels, k_size=3, batch_norm=True, groups=1, dropout=0, with_att=False, with_res=True, out_activation=True, bias=True, global_padding=False, factor=2, down_method='max', depth_embedding_dim=0):
         super().__init__()
 
         self.res_net_blocks = nn.ModuleList()
 
-        self.res_net_blocks.append(res_net_block(hw, in_channels, out_channels, k_size=k_size, batch_norm=batch_norm, groups=groups, dropout=dropout, with_att=with_att, with_res=with_res, out_activation=out_activation, bias=bias, global_padding=global_padding))
+        self.res_net_blocks.append(res_net_block(hw, in_channels, out_channels, k_size=k_size, batch_norm=batch_norm, groups=groups, dropout=dropout, with_att=with_att, with_res=with_res, out_activation=out_activation, bias=bias, global_padding=global_padding, depth_embedding_dim=depth_embedding_dim))
         
         for _ in range(n_blocks-1):
-            self.res_net_blocks.append(res_net_block(hw, out_channels, out_channels, k_size=k_size, batch_norm=batch_norm, groups=groups, dropout=dropout, with_att=False, with_res=with_res, out_activation=out_activation, bias=bias, global_padding=global_padding))
+            self.res_net_blocks.append(res_net_block(hw, out_channels, out_channels, k_size=k_size, batch_norm=batch_norm, groups=groups, dropout=dropout, with_att=False, with_res=with_res, out_activation=out_activation, bias=bias, global_padding=global_padding, depth_embedding_dim=depth_embedding_dim))
 
         if down_method=='max':
             self.pool = nn.MaxPool2d(kernel_size=factor) if factor>1 else nn.Identity()
@@ -124,14 +165,14 @@ class res_blocks(nn.Module):
         elif down_method=='avg':
             self.pool = nn.AvgPool2d(kernel_size=factor) if factor>1 else nn.Identity()
 
-    def forward(self,x):
+    def forward(self, x, depth_emb=None):
         for layer in self.res_net_blocks:
-            x = layer(x)
+            x = layer(x, depth_emb=depth_emb)
 
         return self.pool(x), x
 
 class encoder(nn.Module): 
-    def __init__(self, hw_in, factor, n_levels, n_blocks, u_net_channels, in_channels, k_size=3, k_size_in=7, batch_norm=True, n_groups=1, dropout=0, bias=True, global_padding=False, initial_res=False,down_method='max'):
+    def __init__(self, hw_in, factor, n_levels, n_blocks, u_net_channels, in_channels, k_size=3, k_size_in=7, batch_norm=True, n_groups=1, dropout=0, bias=True, global_padding=False, initial_res=False,down_method='max', depth_embedding_dim=0):
         super().__init__()
 
         hw_in = torch.tensor(hw_in)
@@ -161,19 +202,19 @@ class encoder(nn.Module):
                                  'hw': hw})
             
 
-            self.layers.append(res_blocks(hw, n_blocks, in_channels_block, out_channels_block, k_size=k_size, batch_norm=batch_norm, groups=groups, with_res=with_res, factor=factor_level, dropout=dropout, bias=bias, global_padding=global_padding, down_method=down_method))
+            self.layers.append(res_blocks(hw, n_blocks, in_channels_block, out_channels_block, k_size=k_size, batch_norm=batch_norm, groups=groups, with_res=with_res, factor=factor_level, dropout=dropout, bias=bias, global_padding=global_padding, down_method=down_method, depth_embedding_dim=depth_embedding_dim))
 
     
-    def forward(self, x):
+    def forward(self, x, depth_emb=None):
         outputs = []
 
         for layer in self.layers:
-            x, x_skip = layer(x)
+            x, x_skip = layer(x, depth_emb=depth_emb)
             outputs.append(x_skip)
         return x, outputs
 
 class decoder_block_shuffle(nn.Module):
-    def __init__(self, hw, n_blocks, in_channels, out_channels, skip_channels, k_size=3, dropout=0, upscale_factor=2, bias=True, global_padding=False, out_activation=True, groups=1, with_res=True):
+    def __init__(self, hw, n_blocks, in_channels, out_channels, skip_channels, k_size=3, dropout=0, upscale_factor=2, bias=True, global_padding=False, out_activation=True, groups=1, with_res=True, depth_embedding_dim=0):
         super().__init__()
 
         self.skip_channels = skip_channels
@@ -181,21 +222,21 @@ class decoder_block_shuffle(nn.Module):
         
         in_channels = in_channels + skip_channels
 
-        self.res_blocks = res_blocks(hw, n_blocks, in_channels, out_channels, k_size=k_size, batch_norm=False, groups=groups, dropout=dropout, with_res=with_res, bias=bias, global_padding=global_padding, out_activation=out_activation, factor=1)
+        self.res_blocks = res_blocks(hw, n_blocks, in_channels, out_channels, k_size=k_size, batch_norm=False, groups=groups, dropout=dropout, with_res=with_res, bias=bias, global_padding=global_padding, out_activation=out_activation, factor=1, depth_embedding_dim=depth_embedding_dim)
 
-    def forward(self, x, skip_channels=None):
+    def forward(self, x, skip_channels=None, depth_emb=None):
 
         x = self.up(x)
 
         if self.skip_channels>0:
             x = torch.concat((x, skip_channels), dim=1)
       
-        x = self.res_blocks(x)[0]
+        x = self.res_blocks(x, depth_emb=depth_emb)[0]
 
         return x
 
 class decoder(nn.Module):
-    def __init__(self, layer_configs, factor, n_blocks, out_channels, global_upscale_factor=1, k_size=3, dropout=0, n_groups=1, bias=True, global_padding=False):
+    def __init__(self, layer_configs, factor, n_blocks, out_channels, global_upscale_factor=1, k_size=3, dropout=0, n_groups=1, bias=True, global_padding=False, depth_embedding_dim=0):
         super().__init__()
         # define total number of layers, first n_levels-1 with skip, others not
         #watch out for n_groups in last layer before last
@@ -241,27 +282,27 @@ class decoder(nn.Module):
                     in_channels_block = out_channels_block
 
             hw = hw_in/(factor**(n-1))
-            self.decoder_blocks.append(decoder_block_shuffle(hw, n_blocks, in_channels_block, out_channels_block, skip_channels, k_size=k_size, dropout=dropout, bias=bias, global_padding=global_padding, upscale_factor=upscale_factor, groups=groups, out_activation=out_activation))      
+            self.decoder_blocks.append(decoder_block_shuffle(hw, n_blocks, in_channels_block, out_channels_block, skip_channels, k_size=k_size, dropout=dropout, bias=bias, global_padding=global_padding, upscale_factor=upscale_factor, groups=groups, out_activation=out_activation, depth_embedding_dim=depth_embedding_dim))      
         
-    def forward(self, x, skip_channels):
+    def forward(self, x, skip_channels, depth_emb=None):
 
         for k, layer in enumerate(self.decoder_blocks):
             if k < self.skip_blocks:
                 x_skip = skip_channels[-(k+2)]  
-                x = layer(x, x_skip)
+                x = layer(x, x_skip, depth_emb=depth_emb)
             else:
-                x = layer(x)
+                x = layer(x, depth_emb=depth_emb)
         return x
 
 class mid(nn.Module):
-    def __init__(self, hw, n_blocks, channels, k_size=3,  dropout=0, with_att=False, bias=True, global_padding=False):
+    def __init__(self, hw, n_blocks, channels, k_size=3,  dropout=0, with_att=False, bias=True, global_padding=False, depth_embedding_dim=0):
         super().__init__()
 
-        self.res_blocks = res_blocks(hw, n_blocks, channels, channels, k_size=k_size, batch_norm=False, groups=1,  dropout=dropout, with_att=with_att, bias=bias, global_padding=global_padding, factor=1)
+        self.res_blocks = res_blocks(hw, n_blocks, channels, channels, k_size=k_size, batch_norm=False, groups=1,  dropout=dropout, with_att=with_att, bias=bias, global_padding=global_padding, factor=1, depth_embedding_dim=depth_embedding_dim)
 
-    def forward(self, x):
+    def forward(self, x, depth_emb=None):
 
-        x = self.res_blocks(x)[0]
+        x = self.res_blocks(x, depth_emb=depth_emb)[0]
 
         return x
     
@@ -320,25 +361,36 @@ class out_net(nn.Module):
 
 
 class ResUNet(nn.Module): 
-    def __init__(self, hw_in, hw_out, n_levels, factor, n_res_blocks, model_dim_unet, in_channels, out_channels, res_mode, res_indices, batch_norm=True, k_size=3, in_groups=1, out_groups=1, dropout=0, bias=True, global_padding=False, mid_att=False, initial_res=True, down_method="max"):
+    def __init__(self, hw_in, hw_out, n_levels, factor, n_res_blocks, model_dim_unet, in_channels, out_channels, res_mode, res_indices, batch_norm=True, k_size=3, in_groups=1, out_groups=1, dropout=0, bias=True, global_padding=False, mid_att=False, initial_res=True, down_method="max",depth_embedding_dim=0):
         super().__init__()
 
         global_upscale_factor = int(torch.tensor([(hw_out[0]) // hw_in[0], 1]).max())
 
-        self.encoder = encoder(hw_in, factor, n_levels, n_res_blocks, model_dim_unet, in_channels, k_size, 7, batch_norm=batch_norm, n_groups=in_groups, dropout=dropout, bias=bias, global_padding=global_padding, initial_res=initial_res, down_method=down_method)
-        self.decoder = decoder(self.encoder.layer_configs, factor, n_res_blocks, out_channels, global_upscale_factor=global_upscale_factor, k_size=k_size, dropout=dropout, n_groups=out_groups, bias=bias, global_padding=global_padding)
+        self.encoder = encoder(hw_in, factor, n_levels, n_res_blocks, model_dim_unet, in_channels, k_size, 7, batch_norm=batch_norm, n_groups=in_groups, dropout=dropout, bias=bias, global_padding=global_padding, initial_res=initial_res, down_method=down_method,depth_embedding_dim=depth_embedding_dim)
+        self.decoder = decoder(self.encoder.layer_configs, factor, n_res_blocks, out_channels, global_upscale_factor=global_upscale_factor, k_size=k_size, dropout=dropout, n_groups=out_groups, bias=bias, global_padding=global_padding,depth_embedding_dim=depth_embedding_dim)
 
         self.out_net = out_net(res_indices, hw_in, hw_out, res_mode=res_mode, global_padding=global_padding)
   
         hw_mid = torch.tensor(hw_in)// (factor**(n_levels-1))
-        self.mid = mid(hw_mid, n_res_blocks, model_dim_unet*(2**(n_levels-1)), with_att=mid_att, bias=bias, global_padding=global_padding)
+        self.mid = mid(hw_mid, n_res_blocks, model_dim_unet*(2**(n_levels-1)), with_att=mid_att, bias=bias, global_padding=global_padding,depth_embedding_dim=depth_embedding_dim)
 
-    def forward(self, x):
+        if depth_embedding_dim:
+            self.depth_level_mlp = nn.Sequential(
+                    DepthEncoding(model_dim_unet),
+                    nn.Linear(model_dim_unet, model_dim_unet * 4),
+                    nn.SiLU(),
+                    nn.Linear(model_dim_unet * 4, model_dim_unet)
+                )
+        else:
+            self.depth_level_mlp = None
+        
+    def forward(self, x, depth=None):
+        d = self.depth_level_mlp(depth) if self.depth_level_mlp is not None else None
 
         x_res = x
-        x, layer_outputs = self.encoder(x)
-        x = self.mid(x)
-        x = self.decoder(x, layer_outputs)
+        x, layer_outputs = self.encoder(x, depth_emb=d)
+        x = self.mid(x, depth_emb=d)
+        x = self.decoder(x, layer_outputs, depth_emb=d)
         x = self.out_net(x, x_res)
 
         return {'x': x}
@@ -365,6 +417,8 @@ class core_ResUNet(psm.pyramid_step_model):
         batch_norm = model_settings["batch_norm"]
         mid_att = model_settings["mid_att"] if "mid_att" in model_settings.keys() else False
         full_res = model_settings["full_res"] if "full_res" in model_settings.keys() else False
+
+        depth_embedding_dim = model_settings['model_dim_core'] if "with_depth_embedding" in model_settings.keys() else 0
 
         dropout = 0 if 'dropout' not in model_settings.keys() else model_settings['dropout']
         grouped = False if 'grouped' not in model_settings.keys() else model_settings['grouped']
@@ -397,7 +451,7 @@ class core_ResUNet(psm.pyramid_step_model):
         initial_res = model_settings['initial_res'] if "initial_res" in model_settings.keys() else False
         down_method = model_settings['down_method'] if "down_method" in model_settings.keys() else "max"
 
-        self.core_model = ResUNet(hw_in, hw_out, depth, factor, n_blocks, model_dim_core, input_dim, output_dim, model_settings['res_mode'], res_indices, batch_norm=batch_norm, in_groups=in_groups, out_groups=out_groups, dropout=dropout, bias=bias, global_padding=global_padding, mid_att=mid_att, initial_res=initial_res, down_method=down_method)
+        self.core_model = ResUNet(hw_in, hw_out, depth, factor, n_blocks, model_dim_core, input_dim, output_dim, model_settings['res_mode'], res_indices, batch_norm=batch_norm, in_groups=in_groups, out_groups=out_groups, dropout=dropout, bias=bias, global_padding=global_padding, mid_att=mid_att, initial_res=initial_res, down_method=down_method, depth_embedding_dim=depth_embedding_dim)
 
         if "pretrained_path" in self.model_settings.keys():
             self.check_pretrained(log_dir_check=self.model_settings['pretrained_path'])

climatereconstructionai/model/pyramid_step_model.py
@@ -92,6 +92,8 @@ class output_net(nn.Module):
 
             if non_valid_mask is not None:
                 non_valid_mask_var.update(dict(zip(vars, [non_valid_mask[spatial_dim]]*len(vars))))
+            else:
+                non_valid_mask_var.update(dict(zip(vars, [torch.zeros((data.shape[0],data.shape[-1]), dtype=bool)]*len(vars))))
 
         return data_out, non_valid_mask_var
 
@@ -168,8 +170,9 @@ class pyramid_step_model(nn.Module):
         else:
             self.input_avg_pooling = nn.Identity()
 
-    def forward(self, x, coords_target, coords_source=None, norm=False, apply_res=True):
-    
+    def forward(self, x, coords_target, coords_source=None, norm=False, apply_res=True, depth=None):
+        # coords target: Values from 0 to 1
+
         if norm:
             x = self.normalize(x)
 
@@ -179,23 +182,22 @@ class pyramid_step_model(nn.Module):
         x_reg_lr = x
 
         if not isinstance(self.core_model, nn.Identity):
-            
             x = self.input_avg_pooling(x)
-            output = self.core_model(x)
+            output = self.core_model(x, depth=depth)
             core_output = output
             
-            coords_target_hr, non_valid = helpers.scale_coords(coords_target, self.range_region_target_radx, rngy=self.range_region_target_rady)
-            x, non_valid_var = self.output_net_post(output['x'], coords_target_hr, non_valid)
+            #coords_target_hr, non_valid = helpers.scale_coords(coords_target, self.range_region_target_radx, rngy=self.range_region_target_rady)
+            x, non_valid_var = self.output_net_post(output['x'], coords_target, None)
 
         else:
             core_output = {'x': x}
-            coords_target_hr, non_valid = helpers.scale_coords(coords_target, self.range_region_source_radx, rngy=self.range_region_source_rady)
-            x, non_valid_var = self.output_net_post(x[:,list(self.output_res_indices.values()),:,:], coords_target_hr, non_valid)
+            #coords_target_hr, non_valid = helpers.scale_coords(coords_target, self.range_region_source_radx, rngy=self.range_region_source_rady)
+            x, non_valid_var = self.output_net_post(x[:,list(self.output_res_indices.values()),:,:], coords_target, None)
 
         
         if self.res_mode == 'sample' and not isinstance(self.core_model, nn.Identity):
-            coords_target_lr, non_valid = helpers.scale_coords(coords_target, self.range_region_source_radx, rngy=self.range_region_source_rady)
-            x_pre = self.output_net_pre(x_reg_lr[:,list(self.output_res_indices.values()),:,:], coords_target_lr, non_valid)[0]
+            #coords_target_lr, non_valid = helpers.scale_coords_rel(coords_target, self.coord_scaling_source_target)
+            x_pre = self.output_net_pre(x_reg_lr[:,list(self.output_res_indices.values()),:,:], coords_target, None)[0]
 
             for var in self.output_res_indices.keys():
                 if self.use_gnlll:
@@ -245,15 +247,27 @@ class pyramid_step_model(nn.Module):
         data_source = {}
         coords_source = {}
 
+        range_lon = [float(ds.clon.min()), float(ds.clon.max())]
+        range_lat = [float(ds.clat.min()), float(ds.clat.max())]
+
         patches_target = gu.get_patches(
             self.model_settings["grid_spacing_equator_km"],
             self.model_settings["pix_size_patch"],
-            0)
-        
+            0,
+            range_data_lon=range_lon,
+            range_data_lat=range_lat)
+
+        # only for channel data
+        self.patches_source = gu.get_patches(
+                self.model_settings["grid_spacing_equator_km"],
+                self.model_settings["pix_size_patch"],
+                self.model_settings["patches_overlap_source"],
+                range_data_lon=range_lon,
+                range_data_lat=range_lat)
+
         spatial_dims_patches_source = {}
         spatial_dims_patches_target = {}
         spatial_dims_n_pts = {}
-
 
         #collect all data
         vars_target = self.model_settings['variables_target']
@@ -263,7 +277,7 @@ class pyramid_step_model(nn.Module):
             coord_dict = gu.get_coord_dict_from_var(ds, spatial_dim)
             coords = gu.get_coords_as_tensor(ds, lon=coord_dict['lon'], lat=coord_dict['lat'])
 
-            ids_in_patches_source, patch_ids = gu.get_ids_in_patches(self.patches_source, coords.numpy())
+            ids_in_patches_source, patch_ids = gu.get_ids_in_patches(self.patches_source, coords.numpy(), lon_periodicity=range_lon)
 
             spatial_dims_patches_source[spatial_dim] = ids_in_patches_source
 
@@ -272,7 +286,7 @@ class pyramid_step_model(nn.Module):
             coord_dict = gu.get_coord_dict_from_var(ds_target, spatial_dim)
             coords = gu.get_coords_as_tensor(ds_target, lon=coord_dict['lon'], lat=coord_dict['lat'])
 
-            ids_in_patches_target, patch_ids = gu.get_ids_in_patches(patches_target, coords.numpy())
+            ids_in_patches_target, patch_ids = gu.get_ids_in_patches(patches_target, coords.numpy(), lon_periodicity=range_lon)
             spatial_dims_patches_target[spatial_dim] = ids_in_patches_target
 
         # to batch: patch_indices
@@ -283,8 +297,8 @@ class pyramid_step_model(nn.Module):
             patch_borders_source_lon = self.patches_source["borders_lon"][patch_ids["lon"][int(patch_id_idx)]]
             patch_borders_source_lat = self.patches_source["borders_lat"][patch_ids["lat"][int(patch_id_idx)]]
 
-            patch_borders_target_lon = self.patches_target["borders_lon"][patch_ids["lon"][int(patch_id_idx)]]
-            patch_borders_target_lat = self.patches_target["borders_lat"][patch_ids["lat"][int(patch_id_idx)]]
+            patch_borders_target_lon = patches_target["borders_lon"][patch_ids["lon"][int(patch_id_idx)]]
+            patch_borders_target_lat = patches_target["borders_lat"][patch_ids["lat"][int(patch_id_idx)]]
     
             coords_source = {}
             data_source = {}
@@ -294,9 +308,9 @@ class pyramid_step_model(nn.Module):
                 coords = gu.get_coords_as_tensor(ds, lon=coord_dict['lon'], lat=coord_dict['lat'])
 
                 indices = spatial_dims_patches_source[spatial_dim][patch_id_idx]
-
-                coords_source[spatial_dim] = self.get_coordinates_frame(coords[:,indices], patch_borders_source_lon, patch_borders_source_lat).unsqueeze(dim=0).to(device)
                 
+                coords_source[spatial_dim] = self.get_coordinates_frame(coords[:,indices], patch_borders_source_lon, patch_borders_source_lat, lon_periodicity=range_lon).unsqueeze(dim=0).to(device)
+
                 for variable in vars:
                     data_source[variable] = torch.tensor(ds[variable].values[ts,0,indices]).unsqueeze(dim=-1).unsqueeze(dim=0).to(device)
 
@@ -310,7 +324,7 @@ class pyramid_step_model(nn.Module):
 
                 indices = spatial_dims_patches_target[spatial_dim][patch_id_idx]
 
-                coords_target[spatial_dim] = self.get_coordinates_frame(coords[:,indices], patch_borders_target_lon, patch_borders_target_lat).unsqueeze(dim=0).to(device)
+                coords_target[spatial_dim] = self.get_coordinates_frame(coords[:,indices], patch_borders_source_lon, patch_borders_source_lat, lon_periodicity=range_lon).unsqueeze(dim=0).to(device)
                 var_spatial_dims.update(dict(zip(vars,[spatial_dim]*len(vars))))
 
 
@@ -319,15 +333,16 @@ class pyramid_step_model(nn.Module):
         return data_input, spatial_dims_patches_target, var_spatial_dims
 
     def apply_patches(self, ds, ts=-1, device='cpu', ds_target=None):
-            
-        data_input, spatial_dims_patches_target, var_spatial_dims = self.preprocess_data_patches(ds, ts=ts, device='cpu', ds_target=ds_target)
+        if ds_target is None:
+            ds_target=ds
+        data_input, spatial_dims_patches_target, var_spatial_dims = self.preprocess_data_patches(ds, ts=ts, device=device, ds_target=ds_target)
 
         print(f'prepared data for {len(data_input)} patches')
 
-        output_global = dict(zip(var_spatial_dims.keys(), [torch.tensor(ds_target[variable][0].values).squeeze().to(device) for variable in var_spatial_dims.keys()]))
+        output_global = dict(zip(var_spatial_dims.keys(), [torch.tensor(ds_target[variable][0,0].values).squeeze().to(device) for variable in var_spatial_dims.keys()]))
 
         if self.model_settings['gauss']:
-            output_global_std = dict(zip(var_spatial_dims.keys(), [torch.tensor(ds_target[variable][0].values).squeeze().to(device) for variable in var_spatial_dims.keys()]))
+            output_global_std = dict(zip(var_spatial_dims.keys(), [torch.tensor(ds_target[variable][0,0].values).squeeze().to(device) for variable in var_spatial_dims.keys()]))
         else:
             output_global_std = {}
 
@@ -343,8 +358,12 @@ class pyramid_step_model(nn.Module):
                         apply_res = self.model_settings['apply_res']
                 else:
                     apply_res = True
-                output = self(data_source, coords_target, coords_source=coords_source, norm=True, apply_res=apply_res)[0]
-
+            #    print([var.std() for var in data_source.values()])
+                output, x_reg_lr, core_output, non_valid_var = self(data_source, coords_target, coords_source=coords_source, norm=True, apply_res=apply_res)
+                
+                #debug = {'x_reg_lr':x_reg_lr, 'core_output': core_output, 'non_valid_var': non_valid_var}
+                #torch.save(debug, f'/home/k/k204244/work_exaocean/baroclinic_unet/test/test_nudging_rel/debug_{patch_id_idx}.pt')
+            #    print([var.std() for var in output.values()])
             for variable in output.keys():
                 indices = spatial_dims_patches_target[var_spatial_dims[variable]][patch_id_idx]
                 output_global[variable][indices] = output[variable][0,0,0]
@@ -416,15 +435,17 @@ class pyramid_step_model(nn.Module):
         pass
         
 
-    def get_coordinates_frame(self, coords, patch_borders_lon, patch_borders_lat):
+    def get_coordinates_frame(self, coords, patch_borders_lon, patch_borders_lat, lon_periodicity=[-math.pi, math.pi]):
+        
+        periodic_range = (lon_periodicity[1]-lon_periodicity[0])
 
-        if patch_borders_lon[0] < -math.pi:
-            shift_indices = coords[0,:] > 2*math.pi + patch_borders_lon[0]
-            coords[0,shift_indices] =  coords[0, shift_indices] - 2*math.pi
+        if patch_borders_lon[0] < lon_periodicity[0]:
+            shift_indices = coords[0,:] >= periodic_range + patch_borders_lon[0]
+            coords[0,shift_indices] =  coords[0, shift_indices] - periodic_range
 
-        elif patch_borders_lon[1] > math.pi:
-            shift_indices = coords[0,:] < patch_borders_lon[1] - 2*math.pi
-            coords[0,shift_indices] =  coords[0,shift_indices] + 2*math.pi
+        elif patch_borders_lon[1] > lon_periodicity[1]:
+            shift_indices = coords[0,:] <= patch_borders_lon[1]- periodic_range
+            coords[0,shift_indices] =  coords[0,shift_indices] + periodic_range
 
         rel_coords_lon = (coords[0,:] - patch_borders_lon[0])/(patch_borders_lon[1]-patch_borders_lon[0])
         rel_coords_lat = (coords[1,:] - patch_borders_lat[0])/(patch_borders_lat[1]-patch_borders_lat[0])
@@ -471,54 +492,13 @@ class pyramid_step_model(nn.Module):
         self.radius_region_source_km = self.model_settings['radius_region_source_km']
         self.radius_region_target_km = self.model_settings['radius_region_target_km']
 
-        if self.model_settings['model_type']=="patches_km":
-            self.range_region_source_radx = [-self.radius_region_source_km/(6371), self.radius_region_source_km/(6371)]
-            self.range_region_target_radx = [-self.radius_region_target_km/(6371), self.radius_region_target_km/(6371)]
+        self.n_in = self.model_settings['n_regular'][0]
+        self.n_out = self.model_settings['n_regular'][1]
 
-            self.range_region_source_rady = self.range_region_source_radx
-            self.range_region_target_rady = self.range_region_target_radx
+        patch_overlap_target, patch_overlap_source = self.model_settings["patches_overlap_target"], self.model_settings["patches_overlap_source"]
 
-        elif self.model_settings['model_type']=="global":
-            self.range_region_source_radx = [-math.pi, math.pi]
-            self.range_region_target_radx = [-math.pi, math.pi]
+        self.coord_scaling_source_target = (self.n_out[0] + patch_overlap_target)/(self.n_in[0] + patch_overlap_source)
 
-            self.range_region_source_rady = [-math.pi/2, math.pi/2]
-            self.range_region_target_rady = [-math.pi/2, math.pi/2]
-        
-        elif self.model_settings['model_type']=="patches":
-            self.patches_source = gu.get_patches(
-                self.model_settings["grid_spacing_equator_km"],
-                self.model_settings["pix_size_patch"],
-                self.model_settings["patches_overlap_source"])
-
-            self.patches_target = gu.get_patches(
-                self.model_settings["grid_spacing_equator_km"],
-                self.model_settings["pix_size_patch"],
-                self.model_settings["patches_overlap_target"])
-            
-            range_target_lon = self.patches_target["borders_lon"][0]
-            range_source_lon = self.patches_source["borders_lon"][0]
-           
-
-            range_source_lon_rel = (range_source_lon - range_target_lon[0])/(range_target_lon[1] - range_target_lon[0])
-            self.range_region_source_radx = [range_source_lon_rel[0], range_source_lon_rel[1]]
-            self.range_region_target_radx = [0, 1]
-
-            range_target_lat = self.patches_target["borders_lat"][0]
-            range_source_lat = self.patches_source["borders_lat"][0]
-
-
-            range_source_lat_rel = (range_source_lat - range_target_lat[0])/(range_target_lat[1] - range_target_lat[0])
-            self.range_region_source_rady = [range_source_lat_rel[0], range_source_lat_rel[1]]
-            self.range_region_target_rady = [0, 1]
- 
-        self.n_in, self.n_out = self.model_settings['n_regular']
-
-
-        self.model_settings['range_region_source_radx'] = self.range_region_source_radx
-        self.model_settings['range_region_target_radx'] = self.range_region_target_radx
-        self.model_settings['range_region_source_rady'] = self.range_region_source_rady
-        self.model_settings['range_region_target_rady'] = self.range_region_target_rady
 
     def set_normalizer(self):
         self.normalize = normalizer(self.model_settings['normalization'])
@@ -613,8 +593,6 @@ class pyramid_step_model(nn.Module):
             'interpolate_source': True,
             'interpolate_target': False,
             'interpolation_size_s': self.n_in,
-            'range_source_x': self.range_region_source_radx,
-            'range_source_y': self.range_region_source_rady,
             'interpolation_method': self.model_settings['interpolation_method'] if 'interpolation_method' in self.model_settings else 'nearest' 
             }
 

climatereconstructionai/utils/grid_utils.py
@@ -672,16 +672,15 @@ def generate_region(coords, range_lon=None, range_lat=None, n_points=None, radiu
     return out_dict
 
 
-def get_patches(grid_spacing_equator_km, pix_size, overlap):
-
-    total_grid_size_lon_pix = (2 * math.pi * radius_earth)/grid_spacing_equator_km
+def get_patches(grid_spacing_equator_km, pix_size, overlap, range_data_lon=[-math.pi, math.pi], range_data_lat =[-math.pi/2, math.pi/2]):
+    
+    total_grid_size_lon_pix = ((range_data_lon[1]-range_data_lon[0]) * radius_earth)/grid_spacing_equator_km
     total_grid_size_lon_pix = 2**np.ceil(np.log2(total_grid_size_lon_pix))
-
 
     n_patches_lon = total_grid_size_lon_pix/pix_size
 
-    border_patches_lon = np.linspace(-math.pi, math.pi,int(n_patches_lon)+1)
-    border_patches_lat = np.linspace(-math.pi/2, math.pi/2,int(n_patches_lon/2)+1)
+    border_patches_lon = np.linspace(range_data_lon[0], range_data_lon[1],int(n_patches_lon)+1)
+    border_patches_lat = np.linspace(range_data_lat[0], range_data_lat[1],int(n_patches_lon/2)+1)
 
     centers_lon = (border_patches_lon[1:] + border_patches_lon[:-1])/2
     centers_lat = (border_patches_lat[1:] + border_patches_lat[:-1])/2
@@ -705,12 +704,14 @@ def get_patches(grid_spacing_equator_km, pix_size, overlap):
     return patches
 
 
-def get_ids_in_patches(patches, coords, return_torch=True):
+def get_ids_in_patches(patches, coords, return_torch=True, lon_periodicity=[-math.pi,math.pi]):
 
     centers_lon = patches['centers_lon']
     centers_lat = patches['centers_lat']
     border_patches_lon = patches['borders_lon']
     border_patches_lat = patches['borders_lat']
+
+    periodic_range = lon_periodicity[1]-lon_periodicity[0]
 
     ids_in_patches = []
     patch_ids_lon = []
@@ -724,11 +725,11 @@ def get_ids_in_patches(patches, coords, return_torch=True):
 
             in_patch_lon = np.logical_and(coords[0] >= border_patch_lon[0], coords[0] < border_patch_lon[1])
 
-            if np.round(border_patch_lon[0],6) < np.round(math.pi,6):
-                in_patch_lon = np.logical_or(in_patch_lon, (coords[0] >= (2*math.pi + border_patch_lon[0])))
+            if np.round(border_patch_lon[0],6) < np.round(lon_periodicity[0],6):
+                in_patch_lon = np.logical_or(in_patch_lon, (coords[0] >= (periodic_range + border_patch_lon[0])))
 
-            elif np.round(border_patch_lon[1],6) > np.round(math.pi,6):
-                in_patch_lon = np.logical_or(in_patch_lon, (coords[0] <= (border_patch_lon[1] - 2*math.pi)))
+            elif np.round(border_patch_lon[1],6) > np.round(lon_periodicity[1],6):
+                in_patch_lon = np.logical_or(in_patch_lon, (coords[0] <= (border_patch_lon[1] - periodic_range)))
 
             in_patch_lat = np.logical_and(coords[1] >= border_patch_lat[0], coords[1] < border_patch_lat[1])
             

climatereconstructionai/utils/netcdfloader_patches.py
@@ -74,16 +74,19 @@ def get_dims_coordinates(ds, variables):
     
     spatial_dims = {}
     coord_dicts = {}
+    #depths_dict = {}
     for variable in variables:
         
         coord_dict = get_coord_dict_from_var(ds, variable)
 
         spatial_dims[variable] = coord_dict['spatial_dim']
         coord_dicts[coord_dict['spatial_dim']] = coord_dict
+        #depths_dict[variable] = ds[variable].shape[1]
 
     coord_dict_variables['var_spatial_dims'] = spatial_dims
     coord_dict_variables['coord_dicts'] = coord_dicts
     coord_dict_variables['spatial_dims_var'] = invert_dict(coord_dict_variables['var_spatial_dims'])
+    coord_dict_variables['depths'] = ds[variable].shape[1]
 
 
     return coord_dict_variables
@@ -185,8 +188,15 @@ class NetCDFLoader_lazy(Dataset):
         self.sample_patch_range_lat = sample_patch_range_lat
         self.sample_condition_dict = sample_condition_dict
 
-        self.patches_source = get_patches(grid_spacing_equator_km, pix_size_patch, patches_overlap_source)
-        self.patches_target = get_patches(grid_spacing_equator_km, pix_size_patch, patches_overlap_target)
+        ds_source = xr.open_dataset(files_source[0])
+        ds_target = xr.open_dataset(files_target[0])
+
+        range_lon = [float(ds_source.clon.min()), float(ds_source.clon.max())]
+        range_lat = [float(ds_source.clat.min()), float(ds_source.clat.max())]
+        self.lon_periodicity = range_lon
+
+        self.patches_source = get_patches(grid_spacing_equator_km, pix_size_patch, patches_overlap_source, range_data_lon=range_lon, range_data_lat=range_lat)
+        self.patches_target = get_patches(grid_spacing_equator_km, pix_size_patch, patches_overlap_target, range_data_lon=range_lon, range_data_lat=range_lat)
         
         if interpolation_dict is not None:
                  
@@ -198,8 +208,8 @@ class NetCDFLoader_lazy(Dataset):
                 if interpolation_dict['interpolate_target']:
                     self.input_mapper_t = unstructured_to_reg_interpolator(
                         interpolation_dict['interpolation_size_t'],
-                        interpolation_dict['range_target_x'],
-                        interpolation_dict['range_target_y'],
+                        [0,1],
+                        [0,1],
                         method=interpolation_dict['interpolation_method']
                         )
                     
@@ -218,9 +228,7 @@ class NetCDFLoader_lazy(Dataset):
         self.files_source = files_source
         self.files_target = files_target
 
-        ds_source = xr.open_dataset(files_source[0])
-        ds_target = xr.open_dataset(files_target[0])
-
+        
         self.dims_variables_source = get_dims_coordinates(ds_source, self.variables_source)    
         self.dims_variables_target = get_dims_coordinates(ds_target, self.variables_target)  
 
@@ -255,7 +263,7 @@ class NetCDFLoader_lazy(Dataset):
         self.normalizer = normalizer(self.norm_dict)
 
 
-    def get_ids_patches(self, ds, dims_variables_dict, patches):
+    def get_ids_patches(self, ds, dims_variables_dict, patches, lon_periodicity):
 
         spatial_dims_patches = {}
         spatial_dims_n_pts = {}
@@ -263,18 +271,19 @@ class NetCDFLoader_lazy(Dataset):
             
             coords = get_coords_as_tensor(ds, lon=coord_dict['lon'], lat=coord_dict['lat'])
 
-            ids_in_patches, patch_ids = get_ids_in_patches(patches, coords.numpy())
+            ids_in_patches, patch_ids = get_ids_in_patches(patches, coords.numpy(), lon_periodicity=self.lon_periodicity)
             spatial_dims_patches[spatial_dim] = ids_in_patches
             spatial_dims_n_pts[spatial_dim] = torch.tensor([len(ids_in_patch) for ids_in_patch in ids_in_patches])
 
         return spatial_dims_patches, spatial_dims_n_pts, patch_ids
 
 
-    def get_coordinates(self, ds, dims_variables_dict, spatial_dims_patches_ids, n_dict, patches, patch_id=None):
+    def get_coordinates(self, ds, dims_variables_dict, spatial_dims_patches_ids, n_dict, patches, patch_id=None, lon_periodicity=[-math.pi,math.pi]):
 
         spatial_dim_indices = {}
         rel_coords_dict = {} 
 
+        periodic_range = lon_periodicity[1] - lon_periodicity[0]
         
 
         for spatial_dim, coord_dict in dims_variables_dict['coord_dicts'].items():
@@ -287,6 +296,7 @@ class NetCDFLoader_lazy(Dataset):
 
             if n_pts > len(indices):
                 pad_indices = torch.randint(len(indices), size=(n_pts - len(indices),1)).view(-1)
+              
                 indices = torch.concat((indices, indices[pad_indices]))
             else:    
                 indices = indices[torch.randperm(len(indices-1))[:n_pts]]
@@ -298,13 +308,13 @@ class NetCDFLoader_lazy(Dataset):
 
             spatial_dim_indices[spatial_dim] = indices
 
-            if patch_borders_lon[0] < -math.pi:
-                shift_indices = coords[0,:] > 2*math.pi + patch_borders_lon[0]
-                coords[0,shift_indices] =  coords[0, shift_indices] - 2*math.pi
+            if patch_borders_lon[0] < lon_periodicity[0]:
+                shift_indices = coords[0,:] >= periodic_range + patch_borders_lon[0]
+                coords[0,shift_indices] =  coords[0, shift_indices] - periodic_range
 
-            elif patch_borders_lon[1] > math.pi:
-                shift_indices = coords[0,:] < patch_borders_lon[1] - 2*math.pi
-                coords[0,shift_indices] =  coords[0,shift_indices] + 2*math.pi
+            elif patch_borders_lon[1] > lon_periodicity[1]:
+                shift_indices = coords[0,:] <= patch_borders_lon[1]- periodic_range
+                coords[0,shift_indices] =  coords[0,shift_indices] + periodic_range
 
             rel_coords_lon = (coords[0,:] - patch_borders_lon[0])/(patch_borders_lon[1]-patch_borders_lon[0])
             rel_coords_lat = (coords[1,:] - patch_borders_lat[0])/(patch_borders_lat[1]-patch_borders_lat[0])
@@ -326,11 +336,12 @@ class NetCDFLoader_lazy(Dataset):
             dims = tuple(ds[vars[0]].dims)
            
             time_indices = np.arange(self.num_datapoints_time)
+            depth_indices = np.arange(dims_variables_dict['depths'])
 
             if len(dims)==2:
                 sel_indices = (time_indices, spatial_indices)
             else:
-                sel_indices = (time_indices,[0],spatial_indices)
+                sel_indices = (time_indices,depth_indices,spatial_indices)
             
             sel_dict = dict(zip(dims, sel_indices))
 
@@ -415,7 +426,6 @@ class NetCDFLoader_lazy(Dataset):
                 if self.sample_patch_range_lat[0]<center_lat and self.sample_patch_range_lat[1]>center_lat:
                     in_lat_range = True
 
-           
             spatial_dim_indices_source, rel_coords_dict_source, _ = self.get_coordinates(ds_source, self.dims_variables_source, spatial_dims_patches_ids_source, self.n_dict_source, self.patches_source, patch_id=patch_id)
             spatial_dim_indices_target, rel_coords_dict_target, _ = self.get_coordinates(ds_target, self.dims_variables_target, spatial_dims_patches_ids_target, self.n_dict_target, self.patches_target, patch_id=patch_id)
 
@@ -454,7 +464,7 @@ class NetCDFLoader_lazy(Dataset):
         return ds_source_sampled, ds_target_sampled, patch_id, spatial_dim_indices_source, spatial_dim_indices_target, rot_angle, ds_target_past_sampled, spatial_dim_indices_target_past
 
 
-    def get_data(self, ds, index, dims_variables_dict):
+    def get_data(self, ds, index, dims_variables_dict, depth=0):
 
         data = {}
         coords = {}
@@ -464,7 +474,12 @@ class NetCDFLoader_lazy(Dataset):
             coords[spatial_dim] = get_coords_as_tensor(ds, lon=coord_dict['lon'], lat=coord_dict['lat']).float()
 
             for var in vars:
-                data_var = torch.tensor(ds[var][index].values).squeeze().float()
+                values = torch.tensor(ds[var][index].values)
+                if values.shape[0]>1:
+                    values = values[depth]
+                else:
+                    values = values[0]
+                data_var = values.float().squeeze()
                 data[var] = data_var.unsqueeze(dim=-1)
 
         return data, coords
@@ -476,6 +491,8 @@ class NetCDFLoader_lazy(Dataset):
             if (index < self.index_range_source[0]) or (index > self.index_range_source[1]):
                 index = int(torch.randint(self.index_range_source[0], self.index_range_source[1]+1, (1,1)))
 
+        depth_idx = torch.randint(int(self.dims_variables_source['depths']), (1,1))
+        
         index_target = index + self.index_offset_target
 
         if len(self.files_source)>0:
@@ -487,8 +504,8 @@ class NetCDFLoader_lazy(Dataset):
 
         ds_source, ds_target, patch_id, spatial_dim_indices_source, spatial_dim_indices_target, rot_angle, ds_past, spatial_dim_indices_past = self.get_files(source_file, file_path_target=target_file, file_path_target_past=target_file_past)
 
-        data_source, rel_coords_source = self.get_data(ds_source, index, self.dims_variables_source)
-        data_target, rel_coords_target = self.get_data(ds_target, index_target, self.dims_variables_target)
+        data_source, rel_coords_source = self.get_data(ds_source, index, self.dims_variables_source, depth=depth_idx)
+        data_target, rel_coords_target = self.get_data(ds_target, index_target, self.dims_variables_target, depth=depth_idx)
 
         if ds_past is not None:
             data_past, rel_coords_past = self.get_data(ds_past, index, self.dims_variables_target)
@@ -518,7 +535,7 @@ class NetCDFLoader_lazy(Dataset):
             rel_coords_target = dict(zip(rel_coords_target.keys(),[torch.empty(0) for _ in rel_coords_target.values()]))
 
         if len(self.save_tensor_sample_path)>0:
-            save_path = os.path.join(self.save_tensor_sample_path, os.path.basename(source_file).replace('.nc', f'_{float(patch_id):.3f}_{float(rot_angle):.3f}.pt'))
+            save_path = os.path.join(self.save_tensor_sample_path, os.path.basename(source_file).replace('.nc', f'_{float(patch_id):.3f}_{float(rot_angle):.3f}_{int(depth_idx)}.pt'))
             if not os.path.isfile(save_path):
                 torch.save([data_source, data_target, rel_coords_source, rel_coords_target, spatial_dim_indices_source, spatial_dim_indices_target], save_path)
 
@@ -531,7 +548,7 @@ class NetCDFLoader_lazy(Dataset):
                 with open(os.path.join(self.save_tensor_sample_path,'dims_var_target.json'), 'w') as f:
                     json.dump(self.dims_variables_target, f, indent=4)
 
-        return data_source, data_target, rel_coords_source, rel_coords_target, spatial_dim_indices_source, spatial_dim_indices_target
+        return data_source, data_target, rel_coords_source, rel_coords_target, spatial_dim_indices_source, spatial_dim_indices_target, depth_idx
 
     def __len__(self):
         return self.num_datapoints_time
@@ -541,6 +558,7 @@ class SampleLoader(Dataset):
     def __init__(self, root_dir, dims_variables_source, dims_variables_target, variables_source, variables_target):
         self.root_dir = root_dir
         self.file_list = os.listdir(root_dir)
+        self.file_list = [file_path  for file_path in self.file_list if '.pt' in file_path]
         sample_path = os.path.join(self.root_dir, self.file_list[0])
         coords_source, coords_target = torch.load(sample_path, map_location='cpu')[2:4]
         
@@ -572,6 +590,8 @@ class SampleLoader(Dataset):
                    #     valid_file=False
                 except:
                     idx = torch.randint(0,len(self.file_list), size=(1,))
+        depth = path.split('_')[-1].replace('.pt','')
+        depth = int(depth) if '.' not in depth else 0
 
         source = data[0]
         target = data[1]
@@ -601,6 +621,6 @@ class SampleLoader(Dataset):
             target_indices[spatial_dim] = target_indices[spatial_dim][indices]
             coords_target[spatial_dim] = coords_target[spatial_dim][:,indices]
 
-        data = [source[self.indices_source], target, torch.zeros((10,)), coords_target, target_indices]
+        data = [source[self.indices_source], target, torch.zeros((10,)), coords_target, target_indices, depth]
         
         return data